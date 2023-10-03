9d51ba5d919b5c03c4d225bbc987a26d22d9f731
Sebastian Lopez <sebastian.cardona@benchling.com>
2023-10-03 17:05:04 -0500

    treat nan as None in absolute q reader

--- src/allotropy/parsers/appbio_absolute_q/appbio_absolute_q_reader.py
+++ src/allotropy/parsers/appbio_absolute_q/appbio_absolute_q_reader.py
@@ -1,10 +1,13 @@
 from io import IOBase
 
 import pandas as pd
+import numpy as np
 
 
 class AbsoluteQReader:
     def __init__(self, contents: IOBase):
-        absolute_q_data = pd.read_csv(contents, parse_dates=["Date"])  # type: ignore[call-overload]
+        absolute_q_data = pd.read_csv(contents, parse_dates=["Date"]).replace(
+            np.nan, None
+        )  # type: ignore[call-overload]
         self.wells: pd.DataFrame = absolute_q_data.dropna(subset=["Name"])
         self.group_rows: pd.DataFrame = absolute_q_data[absolute_q_data["Name"].isna()]
