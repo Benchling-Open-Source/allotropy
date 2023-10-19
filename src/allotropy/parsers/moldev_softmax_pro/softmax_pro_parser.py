4258804224c902141d2538345a500afd086106e6
Alejandro Salgado <alejando.salgado@benchling.com>
2023-10-19 11:34:16 -0500

    use lines reader object to read blocks in softmaxpro

--- src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
+++ src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
@@ -1,39 +1,22 @@
+from collections.abc import Iterator
 import io
+import re
 from typing import Any
 
 from allotropy.allotrope.allotrope import AllotropeConversionError
+from allotropy.parsers.lines_reader import LinesReader
 from allotropy.parsers.moldev_softmax_pro.block_factory import create_block
 from allotropy.parsers.moldev_softmax_pro.plate_block import PlateBlock
 from allotropy.parsers.vendor_parser import VendorParser
 
-BLOCKS_LINE_PREFIX = "##BLOCKS= "
-END_LINE_PREFIX = "~End"
+BLOCKS_LINE_REGEX = r"^##BLOCKS=\s*(\d+)$"
+END_LINE_REGEX = "~End"
 
 
 class SoftmaxproParser(VendorParser):
     def _parse(self, contents: io.IOBase, filename: str) -> Any:  # noqa: ARG002
-        file_contents = self._read_contents(contents)
-        lines = file_contents.split("\n")
-
-        # TODO parse whole file, not just blocks
-        blocks_line = lines[0]
-        if not blocks_line.startswith(BLOCKS_LINE_PREFIX):
-            error = f"unrecognized start line {blocks_line}"
-            raise AllotropeConversionError(error)
-
-        num_blocks = int(blocks_line[len(BLOCKS_LINE_PREFIX) :])
-        blocks = []
-        current_block: list[str] = []
-        for line in lines[1:]:
-            if line.startswith(END_LINE_PREFIX):
-                blocks.append(create_block(current_block))
-                current_block = []
-            elif current_block or line.strip():  # drop empty lines between blocks
-                current_block.append(line)
-
-        if len(blocks) != num_blocks:
-            error = f"expected number of blocks to match {num_blocks}"
-            raise AllotropeConversionError(error)
+        lines_reader = LinesReader(contents)
+        blocks = [create_block(block) for block in self._iter_blocks(lines_reader)]
 
         plate_blocks = [block for block in blocks if isinstance(block, PlateBlock)]
         if len(plate_blocks) != 1:
@@ -42,3 +25,16 @@ class SoftmaxproParser(VendorParser):
             error = f"expected exactly 1 plate block, got {block_counts}"
             raise AllotropeConversionError(error)
         return plate_blocks[0].to_allotrope()
+
+    def _get_n_blocks(self, lines_reader: LinesReader) -> int:
+        if search_result := re.search(BLOCKS_LINE_REGEX, lines_reader.pop() or ""):
+            return int(search_result.group(1))
+        error = "unrecognized start line"
+        raise AllotropeConversionError(error)
+
+    def _iter_blocks(self, lines_reader: LinesReader) -> Iterator[list[str]]:
+        n_blocks = self._get_n_blocks(lines_reader)
+        for _ in range(n_blocks):
+            yield list(lines_reader.pop_until(END_LINE_REGEX))
+            lines_reader.pop()  # drop end line
+            lines_reader.drop_empty()
