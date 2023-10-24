ee56b8b168ffe1f90de088ff7729d718fa3c5111
Stephen Worlow <stephen.worlow@benchling.com>
2023-10-24 14:04:15 -0400

    Expose Model

--- src/allotropy/to_allotrope.py
+++ src/allotropy/to_allotrope.py
@@ -13,8 +13,19 @@ def allotrope_from_io(
     vendor_type: VendorType,
     default_timezone: Optional[tzinfo] = None,
 ) -> dict[str, Any]:
+    return serialize_allotrope(
+        allotrope_model_from_io(contents, filename, vendor_type, default_timezone)
+    )
+
+
+def allotrope_model_from_io(
+    contents: io.IOBase,
+    filename: str,
+    vendor_type: VendorType,
+    default_timezone: Optional[tzinfo] = None,
+) -> Any:
     parser = PARSER_FACTORY.create(vendor_type, default_timezone=default_timezone)
-    return serialize_allotrope(parser.to_allotrope(contents, filename))
+    return parser.to_allotrope(contents, filename)
 
 
 def allotrope_from_file(
@@ -24,5 +35,16 @@ def allotrope_from_file(
 ) -> dict[str, Any]:
     with open(filepath, "rb") as f:
         return allotrope_from_io(
+            f, Path(filepath).name, vendor_type, default_timezone=default_timezone
+        )
+
+
+def allotrope_model_from_file(
+    filepath: str,
+    vendor_type: VendorType,
+    default_timezone: Optional[tzinfo] = None,
+) -> dict[str, Any]:
+    with open(filepath, "rb") as f:
+        return allotrope_model_from_io(
             f, Path(filepath).name, vendor_type, default_timezone=default_timezone
         )
