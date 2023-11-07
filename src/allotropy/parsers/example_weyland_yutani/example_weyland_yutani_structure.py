f73fddb1aeb3e37727e7b04bb72a51c6da575405
Greg Wilson <gvwilson@third-bit.com>
2023-11-07 11:04:43 -0500

    fix: fix type complaints from mypy for Weyland-Yutani parser

--- src/allotropy/parsers/example_weyland_yutani/example_weyland_yutani_structure.py
+++ src/allotropy/parsers/example_weyland_yutani/example_weyland_yutani_structure.py
@@ -20,11 +20,11 @@ class BasicAssayInfo:
     checksum: Optional[str]
 
     @staticmethod
-    def create(bottom: pd.DataFrame) -> BasicAssayInfo:
+    def create(bottom: Optional[pd.DataFrame]) -> BasicAssayInfo:
         checksum = (
             None
             if (bottom is None) or (bottom.iloc[0, 0] != "Checksum")
-            else bottom.iloc[0, 1]
+            else str(bottom.iloc[0, 1])
         )
         return BasicAssayInfo(
             protocol_id=PROTOCOL_ID,
@@ -57,15 +57,17 @@ class Plate:
     results: list[Result]
 
     @staticmethod
-    def create(df: pd.DataFrame) -> list[Plate]:
+    def create(df: Optional[pd.DataFrame]) -> list[Plate]:
+        if df is None:
+            return []
         pivoted = df.T
         if pivoted.iloc[1, 0] != "A":
             msg = "Column header(s) not found"
             raise AllotropeConversionError(msg)
         stripped = pivoted.drop(0, axis=0).drop(0, axis=1)
         rows, cols = stripped.shape
-        stripped.index = [df.iloc[0, i + 1] for i in range(rows)]  # type: ignore[assignment]
-        stripped.columns = [str(int(df.iloc[i, 0])) for i in range(1, cols + 1)]  # type: ignore[assignment]
+        stripped.index = [df.iloc[0, i + 1] for i in range(rows)]  # type: ignore
+        stripped.columns = [str(int(df.iloc[i, 0])) for i in range(1, cols + 1)]  # type: ignore
         return [
             Plate(
                 number="0",
