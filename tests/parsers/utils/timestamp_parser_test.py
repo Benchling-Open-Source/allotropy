ddbf9f2f4932c6ce8e46548484ffe8d6e5c8c7ec
Brian Worth <brian@benchling.com>
2023-11-10 13:14:12 -0800

    Test TimestampParser.parse() with Nones (#114)
    Test existing contract before changing it.

--- tests/parsers/utils/timestamp_parser_test.py
+++ tests/parsers/utils/timestamp_parser_test.py
@@ -1,4 +1,5 @@
 from datetime import timedelta, timezone, tzinfo
+from typing import Optional
 from zoneinfo import ZoneInfo
 
 import pytest
@@ -18,6 +19,8 @@ def test_timestamp_parser_init_fails_invalid_default_timezone() -> None:
 @pytest.mark.parametrize(
     "time_str,expected",
     [
+        (None, None),
+        ("unparseable", None),
         ("10-11-08", "2008-10-11T00:00:00+00:00"),
         ("Fri, 11 Nov 2011 03:18:09", "2011-11-11T03:18:09+00:00"),
         ("Fri, 11 Nov 2011 03:18:09 -0400", "2011-11-11T03:18:09-04:00"),
@@ -26,7 +29,9 @@ def test_timestamp_parser_init_fails_invalid_default_timezone() -> None:
         ("Tue Jun 22 07:46:22 GMT 2010", "2010-06-22T07:46:22+00:00"),
     ],
 )
-def test_timestamp_parser_default_utc(time_str: str, expected: str) -> None:
+def test_timestamp_parser_default_utc(
+    time_str: Optional[str], expected: Optional[str]
+) -> None:
     assert TimestampParser().parse(time_str) == expected
 
 
