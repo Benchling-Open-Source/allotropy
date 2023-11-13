6b363f5515e53f91dddd8847054972c59b1cab68
Brian Worth <brian@benchling.com>
2023-11-13 11:09:34 -0800

    Fail fast for VendorParser.get_date_time(None) (#116)
    Also, inline a public method that's only used by another public method.

--- tests/parsers/utils/timestamp_parser_test.py
+++ tests/parsers/utils/timestamp_parser_test.py
@@ -19,7 +19,6 @@ def test_timestamp_parser_init_fails_invalid_default_timezone() -> None:
 @pytest.mark.parametrize(
     "time_str,expected",
     [
-        (None, None),
         ("unparseable", None),
         ("10-11-08", "2008-10-11T00:00:00+00:00"),
         ("Fri, 11 Nov 2011 03:18:09", "2011-11-11T03:18:09+00:00"),
@@ -29,9 +28,7 @@ def test_timestamp_parser_init_fails_invalid_default_timezone() -> None:
         ("Tue Jun 22 07:46:22 GMT 2010", "2010-06-22T07:46:22+00:00"),
     ],
 )
-def test_timestamp_parser_default_utc(
-    time_str: Optional[str], expected: Optional[str]
-) -> None:
+def test_timestamp_parser_default_utc(time_str: str, expected: Optional[str]) -> None:
     assert TimestampParser().parse(time_str) == expected
 
 
