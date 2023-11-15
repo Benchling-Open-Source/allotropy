dd5003734189d485625b944a47adcbf5a5342d70
alejandro-salgado <136857397+alejandro-salgado@users.noreply.github.com>
2023-11-15 14:00:03 -0500

    refac: restructure Agilent Gen5 with explicit dataclasses structure (#107)
    - Refactor Agilent Gen5 with explicit dataclasses structure
    
    ---------
    
    Co-authored-by: Nathan Stender <nathan.stender@benchling.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 ### Fixed
 ### Changed
+- Refactor Agilent Gen5 with explicit dataclasses structure
 ### Deprecated
 ### Removed
 ### Security
---
+++ src/allotropy/parsers/agilent_gen5/agilent_gen5_structure.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from allotropy.allotrope.allotrope import AllotropeConversionError
+from allotropy.parsers.agilent_gen5.plate_data import PlateData
+from allotropy.parsers.agilent_gen5.section_reader import SectionLinesReader
+
+
+@dataclass
+class Data:
+    plates: list[PlateData]
+
+    @staticmethod
+    def create(section_lines_reader: SectionLinesReader) -> Data:
+        plates: list[PlateData] = [
+            PlateData.create(lines_reader)
+            for lines_reader in section_lines_reader.iter_sections("^Software Version")
+        ]
+
+        if not plates:
+            msg = "No plate data found in file"
+            raise AllotropeConversionError(msg)
+
+        return Data(plates)
