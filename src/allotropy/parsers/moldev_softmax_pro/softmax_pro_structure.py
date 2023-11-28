3b9f675cfcac01c79496c75b884f76e0ba17f7b7
Alejandro Salgado <alejando.salgado@benchling.com>
2023-11-28 09:34:35 -0500

    add function to select plate block subclass in softmax structure

--- src/allotropy/parsers/moldev_softmax_pro/softmax_pro_structure.py
+++ src/allotropy/parsers/moldev_softmax_pro/softmax_pro_structure.py
@@ -220,58 +220,60 @@ class PlateBlock(Block):
             msg="Unable to get plate block header",
         )
         header_series = raw_header_series.replace("", None).str.strip()
-        read_mode = header_series[5]
 
+        cls = PlateBlock.get_plate_block_cls(header_series)
+        header = cls.parse_header(header_series)
+
+        well_data: defaultdict[str, WellData] = defaultdict(WellData.create)
+
+        split_lines = [
+            [value_or_none(value) for value in raw_line.split("\t")]
+            for raw_line in lines_reader.lines
+        ]
+        data_header = split_lines[1]
+        data_lines = split_lines[2:]
+
+        if header.export_format == ExportFormat.TIME_FORMAT.value:
+            PlateBlock._parse_time_format_data(
+                header,
+                well_data,
+                data_header,
+                data_lines,
+            )
+        elif header.export_format == ExportFormat.PLATE_FORMAT.value:
+            PlateBlock._parse_plate_format_data(
+                header,
+                well_data,
+                data_header,
+                data_lines,
+            )
+        else:
+            error = f"unrecognized export format {header.export_format}"
+            raise AllotropeConversionError(error)
+
+        return cls(
+            block_type="Plate",
+            raw_lines=lines_reader.lines,
+            header=header,
+            well_data=well_data,
+            data_header=data_header,
+        )
+
+    @staticmethod
+    def get_plate_block_cls(header_series: pd.Series[str]) -> type[PlateBlock]:
         plate_block_cls: dict[str, type[PlateBlock]] = {
             "Absorbance": AbsorbancePlateBlock,
             "Fluorescence": FluorescencePlateBlock,
             "Luminescence": LuminescencePlateBlock,
         }
-
-        if cls := plate_block_cls.get(read_mode or ""):
-            header = cls.parse_header(header_series)
-
-            well_data: defaultdict[str, WellData] = defaultdict(WellData.create)
-
-            split_lines = [
-                [value_or_none(value) for value in raw_line.split("\t")]
-                for raw_line in lines_reader.lines
-            ]
-            data_header = split_lines[1]
-            data_lines = split_lines[2:]
-
-            if header.export_format == ExportFormat.TIME_FORMAT.value:
-                PlateBlock._parse_time_format_data(
-                    header,
-                    well_data,
-                    data_header,
-                    data_lines,
-                )
-            elif header.export_format == ExportFormat.PLATE_FORMAT.value:
-                PlateBlock._parse_plate_format_data(
-                    header,
-                    well_data,
-                    data_header,
-                    data_lines,
-                )
-            else:
-                error = msg_for_error_on_unrecognized_value(
-                    "export format", header.export_format, ExportFormat._member_names_
-                )
-                raise AllotropeConversionError(error)
-
-            return cls(
-                block_type="Plate",
-                raw_lines=lines_reader.lines,
-                header=header,
-                well_data=well_data,
-                data_header=data_header,
+        read_mode = header_series[5]
+        cls = plate_block_cls.get(read_mode or "")
+        if cls is None:
+            msg = msg_for_error_on_unrecognized_value(
+                "read mode", read_mode, plate_block_cls.keys()
             )
-
-        msg = msg_for_error_on_unrecognized_value(
-            "read mode", read_mode, plate_block_cls.keys()
-        )
-        raise AllotropeConversionError(msg)
+            raise AllotropeConversionError(msg)
+        return cls
 
     @staticmethod
     def _parse_reduced_plate_rows(
