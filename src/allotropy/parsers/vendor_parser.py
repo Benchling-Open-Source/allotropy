7d48c1d18cb9e34756a16cbd66d6acca4418d436
Brian Worth <brian@benchling.com>
2023-11-30 11:51:28 -0500

    todo

--- src/allotropy/parsers/vendor_parser.py
+++ src/allotropy/parsers/vendor_parser.py
@@ -30,7 +30,7 @@ class VendorParser(ABC):
     def _parse(self, contents: io.IOBase, filename: str) -> Any:
         raise NotImplementedError
 
-    # TODO: rename to _get_date_time
+    # TODO: rename to _get_date_time and make time param a str
     def get_date_time(self, time: Any) -> TDateTimeValue:
         assert_not_none(time, "time")
 
