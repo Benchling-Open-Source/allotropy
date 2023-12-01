7bdd17018ca7f2b39aae62916df18ad29dc839bc
Sebastian Lopez <sebastian.cardona@benchling.com>
2023-12-01 14:01:21 -0500

    CONSTANT_CASE for output_files

--- tests/parsers/unchained_labs_lunatic/unchained_labs_lunatic_parser_tests.py
+++ tests/parsers/unchained_labs_lunatic/unchained_labs_lunatic_parser_tests.py
@@ -3,7 +3,7 @@ import pytest
 from allotropy.parser_factory import Vendor
 from tests.parsers.test_utils import from_file, validate_contents, validate_schema
 
-output_files = (
+OUTPUT_FILES = (
     "Demo_A260_dsDNA_Data",
     "Demo_A280_Protein",
 )
@@ -12,7 +12,7 @@ VENDOR_TYPE = Vendor.UNCHAINED_LABS_LUNATIC
 SCHEMA_FILE = "plate-reader/BENCHLING/2023/09/plate-reader.json"
 
 
-@pytest.mark.parametrize("output_file", output_files)
+@pytest.mark.parametrize("output_file", OUTPUT_FILES)
 def test_parse_cedex_bioht_to_asm(output_file: str) -> None:
     test_filepath = f"tests/parsers/roche_cedex_bioht/testdata/{output_file}.csv"
     expected_filepath = f"tests/parsers/roche_cedex_bioht/testdata/{output_file}.json"
