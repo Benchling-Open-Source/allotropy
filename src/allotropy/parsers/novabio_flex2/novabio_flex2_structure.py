cf6fa287290e64ad592d4f3603c71f4222754776
Brian Worth <brian@benchling.com>
2023-12-07 11:09:44 -0500

    Improve IOType

--- src/allotropy/parsers/novabio_flex2/novabio_flex2_structure.py
+++ src/allotropy/parsers/novabio_flex2/novabio_flex2_structure.py
@@ -137,5 +137,5 @@ class Data:
 
     @staticmethod
     def create(contents: IOType, filename: str) -> Data:
-        data = pd.read_csv(contents, parse_dates=["Date & Time"]).replace(np.nan, None)  # type: ignore[call-overload]
+        data = pd.read_csv(contents, parse_dates=["Date & Time"]).replace(np.nan, None)
         return Data(Title.create(filename), SampleList.create(data))
