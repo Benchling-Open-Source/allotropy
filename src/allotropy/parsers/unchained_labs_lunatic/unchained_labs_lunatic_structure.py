1d0a61c6e50254bdcaec8a446686c1ca2ac9bb97
Sebastian Lopez <sebastian.cardona@benchling.com>
2023-12-13 11:55:38 -0500

    add calculated data documents

--- src/allotropy/parsers/unchained_labs_lunatic/unchained_labs_lunatic_structure.py
+++ src/allotropy/parsers/unchained_labs_lunatic/unchained_labs_lunatic_structure.py
@@ -8,6 +8,7 @@ import pandas as pd
 
 from allotropy.exceptions import AllotropeConversionError
 from allotropy.parsers.unchained_labs_lunatic.constants import (
+    CALCULATED_DATA_LOOKUP,
     INCORRECT_WAVELENGTH_COLUMN_FORMAT_ERROR_MSG,
     NO_DATE_OR_TIME_ERROR_MSG,
     NO_MEASUREMENT_IN_PLATE_ERROR_MSG,
@@ -16,19 +17,36 @@ from allotropy.parsers.unchained_labs_lunatic.constants import (
 )
 from allotropy.parsers.utils.values import (
     try_float_from_series,
+    try_float_from_series_or_none,
     try_str_from_series,
     try_str_from_series_or_none,
 )
 
 
 @dataclass(frozen=True)
+class DataSourceItem:
+    identifier: str
+    feature: str
+
+
+@dataclass(frozen=True)
+class CalculatedDataItem:
+    identifier: str
+    name: str
+    value: float
+    unit: str
+    data_source_document: list[DataSourceItem]
+
+
+@dataclass(frozen=True)
 class Measurement:
-    measurement_identifier: str
+    identifier: str
     wavelength: float
     absorbance: float
     sample_identifier: str
     location_identifier: str
     well_plate_identifier: Optional[str]
+    calculated_data: list[CalculatedDataItem]
 
     @staticmethod
     def create(well_plate_data: pd.Series[Any], wavelength_column: str) -> Measurement:
@@ -39,8 +57,9 @@ class Measurement:
         if not WAVELENGTH_COLUMNS_RE.match(wavelength_column):
             raise AllotropeConversionError(INCORRECT_WAVELENGTH_COLUMN_FORMAT_ERROR_MSG)
 
+        measurement_identifier = str(uuid.uuid4())
         return Measurement(
-            measurement_identifier=str(uuid.uuid4()),
+            identifier=measurement_identifier,
             wavelength=float(wavelength_column[1:]),
             absorbance=try_float_from_series(well_plate_data, wavelength_column),
             sample_identifier=try_str_from_series(well_plate_data, "Sample name"),
@@ -48,7 +67,42 @@ class Measurement:
             well_plate_identifier=try_str_from_series_or_none(
                 well_plate_data, "Plate Position"
             ),
+            calculated_data=Measurement._get_calculated_data(
+                well_plate_data, wavelength_column, measurement_identifier
+            ),
         )
+
+    @staticmethod
+    def _get_calculated_data(
+        well_plate_data: pd.Series[Any],
+        wavelength_column: str,
+        measurement_identifier: str,
+    ) -> list[CalculatedDataItem]:
+        calculated_data_dict = CALCULATED_DATA_LOOKUP.get(wavelength_column)
+        if not calculated_data_dict:
+            return []
+
+        calculated_data: list[CalculatedDataItem] = []
+        for item in calculated_data_dict:
+            value = try_float_from_series_or_none(well_plate_data, item["column"])
+            if value is None:
+                continue
+
+            calculated_data.append(
+                CalculatedDataItem(
+                    identifier=str(uuid.uuid4()),
+                    name=item["name"],
+                    value=value,
+                    unit=item["unit"],
+                    data_source_document=[
+                        DataSourceItem(
+                            identifier=measurement_identifier,
+                            feature=item["feature"],
+                        )
+                    ],
+                )
+            )
+        return calculated_data
 
 
 @dataclass(frozen=True)
@@ -107,3 +161,11 @@ class Data:
         device_identifier = try_str_from_series(data, "Instrument ID")
 
         return str(device_identifier)
+
+    def get_calculated_data_document(self) -> list[CalculatedDataItem]:
+        calculated_data_document = []
+        for well_plate in self.well_plate_list:
+            for measurement in well_plate.measurements:
+                calculated_data_document.extend(measurement.calculated_data)
+
+        return calculated_data_document
