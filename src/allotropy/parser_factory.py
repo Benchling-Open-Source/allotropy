f93156f34668950a04f8ebeed82d3489e35edf9a
Keith Macaulay <keith.macaulay@benchling.com>
2023-12-13 15:51:50 +0000

    duplicating existing quantstudio adapter

--- src/allotropy/parser_factory.py
+++ src/allotropy/parser_factory.py
@@ -10,6 +10,9 @@ from allotropy.parsers.appbio_absolute_q.appbio_absolute_q_parser import (
 from allotropy.parsers.appbio_quantstudio.appbio_quantstudio_parser import (
     AppBioQuantStudioParser,
 )
+from allotropy.parsers.appbio_quantstudio_designandanalysis.appbio_quantstudio_designandanalysis_parser import (
+    AppBioQuantStudioDesignandanalysisParser,
+)
 from allotropy.parsers.beckman_vi_cell_blu.vi_cell_blu_parser import ViCellBluParser
 from allotropy.parsers.beckman_vi_cell_xr.vi_cell_xr_parser import ViCellXRParser
 from allotropy.parsers.chemometec_nucleoview.nucleoview_parser import (
@@ -44,6 +47,7 @@ class Vendor(Enum):
     AGILENT_GEN5 = "AGILENT_GEN5"
     APPBIO_ABSOLUTE_Q = "APPBIO_ABSOLUTE_Q"
     APPBIO_QUANTSTUDIO = "APPBIO_QUANTSTUDIO"
+    APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS = "APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS"
     BECKMAN_VI_CELL_BLU = "BECKMAN_VI_CELL_BLU"
     BECKMAN_VI_CELL_XR = "BECKMAN_VI_CELL_XR"
     CHEMOMETEC_NUCLEOVIEW = "CHEMOMETEC_NUCLEOVIEW"
@@ -62,6 +66,7 @@ _VENDOR_TO_PARSER: dict[Vendor, type[VendorParser]] = {
     Vendor.AGILENT_GEN5: AgilentGen5Parser,
     Vendor.APPBIO_ABSOLUTE_Q: AppbioAbsoluteQParser,
     Vendor.APPBIO_QUANTSTUDIO: AppBioQuantStudioParser,
+    Vendor.APPBIO_QUANTSTUDIO_DESIGNANDANALYSIS: AppBioQuantStudioDesignandanalysisParser,
     Vendor.BECKMAN_VI_CELL_BLU: ViCellBluParser,
     Vendor.BECKMAN_VI_CELL_XR: ViCellXRParser,
     Vendor.CHEMOMETEC_NUCLEOVIEW: ChemometecNucleoviewParser,
---
+++ src/allotropy/parsers/appbio_quantstudio_designandanalysis/views.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from collections.abc import Iterator
+from typing import Generic, Optional, TypeVar, Union
+
+from allotropy.exceptions import AllotropeConversionError
+
+T = TypeVar("T")
+
+
+class ViewData(Generic[T]):
+    def __init__(self, data: dict[str, Union[ViewData[T], list[T]]]):
+        self.data = data
+
+    def get_first_key(self) -> str:
+        key, *_ = self.data.keys()
+        return key
+
+    def iter_keys(self) -> Iterator[list[str]]:
+        for key, item in self.data.items():
+            if isinstance(item, ViewData):
+                for sub_keys in item.iter_keys():
+                    yield [key, *sub_keys]
+            else:
+                yield [key]
+
+    def get_item(self, *keys: str) -> Union[ViewData[T], list[T]]:
+        if len(keys) == 0:
+            return self.data[self.get_first_key()]
+
+        key, *sub_keys = keys
+        item = self.data[key]
+        if isinstance(item, ViewData):
+            return item.get_item(*sub_keys)
+        else:
+            return item
+
+    def get_sub_view_data(self, *keys: str) -> ViewData[T]:
+        item = self.get_item(*keys)
+        if isinstance(item, ViewData):
+            return item
+        msg = f"Unable to find sub view data with keys: {keys}."
+        raise AllotropeConversionError(msg)
+
+    def get_leaf_item(self, *keys: str) -> list[T]:
+        item = self.get_item(*keys)
+        if isinstance(item, ViewData):
+            msg = f"Unable to find leaf item of view data with keys: {keys}."
+            raise AllotropeConversionError(msg)
+        return item
+
+
+class View(Generic[T]):
+    def __init__(self, sub_view: Optional[View[T]] = None):
+        self.sub_view = sub_view
+
+    def sort_elements(self, _: list[T]) -> dict[str, list[T]]:
+        return {}
+
+    def apply(self, elements: list[T]) -> ViewData[T]:
+        return ViewData(
+            {
+                id_: self.sub_view.apply(element) if self.sub_view else element
+                for id_, element in self.sort_elements(elements).items()
+            }
+        )
