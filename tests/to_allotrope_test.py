563d66fe10068538f0c28a77ba12fef3a29e9dbe
Brian Worth <brian@benchling.com>
2023-12-15 15:34:21 -0800

    Use allotrope_model_from_file (#200)
    Make `allotrope_from_file` delegate to `allotrope_model_from_file`.

--- tests/to_allotrope_test.py
+++ tests/to_allotrope_test.py
@@ -1,5 +1,3 @@
-import re
-
 import pytest
 
 from allotropy.exceptions import AllotropeConversionError
@@ -16,8 +14,5 @@ def test_allotrope_from_file_not_found() -> None:
 
 
 def test_allotrope_model_from_file_not_found() -> None:
-    expected_msg = "[Errno 2] No such file or directory: 'not/a/path'"
-    expected_regex = re.escape(expected_msg)
-    # TODO: Should raise AllotropeConversionError
-    with pytest.raises(FileNotFoundError, match=expected_regex):
+    with pytest.raises(AllotropeConversionError, match=EXPECTED_ERROR_MESSAGE):
         allotrope_model_from_file(INVALID_FILE_PATH, Vendor.AGILENT_GEN5)
