9861c2d28e9aaa3481c4683500b9bf651074ad91
Joe Negri <104152759+LimulusPolyphemus@users.noreply.github.com>
2023-12-18 17:31:34 -0500

    Additional mass concentration tQuantityValue for custom.json (#204)
    Addition of modified tQuantityValue for mass concentations: ng/uL,
    ug/uL, ng/mL, ug/mL, mg/mL
    
    ---------
    
    Signed-off-by: Joe Negri <104152759+LimulusPolyphemus@users.noreply.github.com>

--- src/allotropy/allotrope/models/shared/definitions/custom.py
+++ src/allotropy/allotrope/models/shared/definitions/custom.py
@@ -11,15 +11,20 @@ from allotropy.allotrope.models.shared.definitions.units import (
     DegreeCelsius,
     GramPerLiter,
     Hertz,
+    MicrogramPerMicroliter,
+    MicrogramPerMilliliter,
     Microliter,
     Micrometer,
     MilliAbsorbanceUnit,
+    MilligramPerMilliliter,
     Millimeter,
     MillimeterOfMercury,
     MillimolePerLiter,
     MillionCellsPerMilliliter,
     MilliOsmolesPerKilogram,
     MilliSecond,
+    NanogramPerMicroliter,
+    NanogramPerMilliliter,
     Nanometer,
     Number,
     NumberPerMicroliter,
@@ -131,8 +136,43 @@ class TQuantityValueNumber(Number, TQuantityValueWithOptionalUnit):
 
 
 @dataclass
+class TQuantityValueNanogramPerMicroliter(
+    NanogramPerMicroliter, TQuantityValueWithOptionalUnit
+):
+    pass
+
+
+@dataclass
+class TQuantityValueMicrogramPerMicroliter(
+    MicrogramPerMicroliter, TQuantityValueWithOptionalUnit
+):
+    pass
+
+
+@dataclass
 class TQuantityValuePicogramPerMilliliter(
     PicogramPerMilliliter, TQuantityValueWithOptionalUnit
+):
+    pass
+
+
+@dataclass
+class TQuantityValueNanogramPerMilliliter(
+    NanogramPerMilliliter, TQuantityValueWithOptionalUnit
+):
+    pass
+
+
+@dataclass
+class TQuantityValueMicrogramPerMilliliter(
+    MicrogramPerMilliliter, TQuantityValueWithOptionalUnit
+):
+    pass
+
+
+@dataclass
+class TQuantityValueMilligramPerMilliliter(
+    MilligramPerMilliliter, TQuantityValueWithOptionalUnit
 ):
     pass
 
