2d9dc0cb1bb00f8a4e1ca9eebfc6ad78d243016a
Alejandro Salgado <alejando.salgado@benchling.com>
2023-12-20 15:28:27 -0500

    reorganize softmax tests

--- tests/parsers/moldev_softmax_pro/to_allotrope_test.py
+++ tests/parsers/moldev_softmax_pro/to_allotrope_test.py
@@ -10,33 +10,18 @@ VENDOR_TYPE = Vendor.MOLDEV_SOFTMAX_PRO
 
 
 @pytest.mark.parametrize(
-    "test_file,schema_file,expected_file",
+    "test_file,expected_file",
     [
         (
-            "tests/parsers/moldev_softmax_pro/testdata/fl_kinetic_plates.txt",
-            "fluorescence/BENCHLING/2023/09/fluorescence.json",
-            "tests/parsers/moldev_softmax_pro/testdata/expected_fl_kinetic.json",
-        ),
-        (
-            "tests/parsers/moldev_softmax_pro/testdata/lum_spectrum_columns.txt",
-            "luminescence/BENCHLING/2023/09/luminescence.json",
-            "tests/parsers/moldev_softmax_pro/testdata/expected_lum_spectrum.json",
-        ),
-        (
             "tests/parsers/moldev_softmax_pro/testdata/abs_endpoint_plates.txt",
-            "ultraviolet-absorbance/BENCHLING/2023/09/ultraviolet-absorbance.json",
             "tests/parsers/moldev_softmax_pro/testdata/expected_abs_endpoint.json",
         ),
     ],
 )
-def test_to_allotrope(test_file: str, schema_file: str, expected_file: str) -> None:
+def test_to_allotrope(test_file: str, expected_file: str) -> None:
     allotrope_dict = from_file(test_file, VENDOR_TYPE)
-    validate_schema(allotrope_dict, schema_file)
+    validate_schema(allotrope_dict, "plate-reader/BENCHLING/2023/09/plate-reader.json")
     validate_contents(allotrope_dict, expected_file)
-    assert (
-        len(allotrope_dict["measurement aggregate document"]["measurement document"])
-        == 96
-    )
 
 
 def test_handles_unrecognized_read_mode() -> None:
@@ -50,3 +35,18 @@ def test_handles_unrecognized_read_mode() -> None:
             "tests/parsers/moldev_softmax_pro/testdata/trf_well_scan_plates.txt",
             VENDOR_TYPE,
         )
+
+
+@pytest.mark.parametrize(
+    "test_file",
+    [
+        "tests/parsers/moldev_softmax_pro/testdata/fl_kinetic_plates.txt",
+        "tests/parsers/moldev_softmax_pro/testdata/lum_spectrum_columns.txt",
+    ],
+)
+def test_unrecognized_read_type(test_file: str) -> None:
+    with pytest.raises(
+        AllotropeConversionError,
+        match="Only Endpoint measurements can be processed at this time.",
+    ):
+        from_file(test_file, VENDOR_TYPE)
