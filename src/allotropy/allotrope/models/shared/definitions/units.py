e9ccbb83513c29f6e2bbca57eb01859dbdc97835
Brian Worth <brian@benchling.com>
2024-01-11 14:43:56 -0500

    Extract HasUnit class

--- src/allotropy/allotrope/models/shared/definitions/units.py
+++ src/allotropy/allotrope/models/shared/definitions/units.py
@@ -3,170 +3,175 @@ from typing import Optional
 
 
 @dataclass
-class Centimeter:
+class HasUnit:
+    unit: Optional[str]
+
+
+@dataclass
+class Centimeter(HasUnit):
     unit: Optional[str] = "cm"
 
 
 @dataclass
-class Millimeter:
+class Millimeter(HasUnit):
     unit: Optional[str] = "mm"
 
 
 @dataclass
-class Nanometer:
+class Nanometer(HasUnit):
     unit: Optional[str] = "nm"
 
 
 @dataclass
-class Micrometer:
+class Micrometer(HasUnit):
     unit: Optional[str] = "μm"
 
 
 @dataclass
-class Hertz:
+class Hertz(HasUnit):
     unit: Optional[str] = "Hz"
 
 
 @dataclass
-class CubicMillimeter:
+class CubicMillimeter(HasUnit):
     unit: Optional[str] = "mm^3"
 
 
 @dataclass
-class Unitless:
+class Unitless(HasUnit):
     unit: Optional[str] = "(unitless)"
 
 
 @dataclass
-class SecondTime:
+class SecondTime(HasUnit):
     unit: Optional[str] = "s"
 
 
 @dataclass
-class Percent:
+class Percent(HasUnit):
     unit: Optional[str] = "%"
 
 
 @dataclass
-class Cell:
+class Cell(HasUnit):
     unit: Optional[str] = "cell"
 
 
 @dataclass
-class Microliter:
+class Microliter(HasUnit):
     unit: Optional[str] = "μL"
 
 
 @dataclass
-class NumberPerMicroliter:
+class NumberPerMicroliter(HasUnit):
     unit: Optional[str] = "#/μL"
 
 
 @dataclass
-class MilliSecond:
+class MilliSecond(HasUnit):
     unit: Optional[str] = "ms"
 
 
 @dataclass
-class MillionCellsPerMilliliter:
+class MillionCellsPerMilliliter(HasUnit):
     unit: Optional[str] = "10^6 cells/mL"
 
 
 @dataclass
-class TODO:
+class TODO(HasUnit):
     unit: Optional[str] = "TODO"
 
 
 @dataclass
-class DegreeCelsius:
+class DegreeCelsius(HasUnit):
     unit: Optional[str] = "degC"
 
 
 @dataclass
-class Number:
+class Number(HasUnit):
     unit: Optional[str] = "#"
 
 
 @dataclass
-class NanogramPerMicroliter:
+class NanogramPerMicroliter(HasUnit):
     unit: Optional[str] = "ng/uL"
 
 
 @dataclass
-class MicrogramPerMicroliter:
+class MicrogramPerMicroliter(HasUnit):
     unit: Optional[str] = "ug/uL"
 
 
 @dataclass
-class PicogramPerMilliliter:
+class PicogramPerMilliliter(HasUnit):
     unit: Optional[str] = "pg/mL"
 
 
 @dataclass
-class NanogramPerMilliliter:
+class NanogramPerMilliliter(HasUnit):
     unit: Optional[str] = "ng/mL"
 
 
 @dataclass
-class MicrogramPerMilliliter:
+class MicrogramPerMilliliter(HasUnit):
     unit: Optional[str] = "ug/mL"
 
 
 @dataclass
-class MilligramPerMilliliter:
+class MilligramPerMilliliter(HasUnit):
     unit: Optional[str] = "mg/mL"
 
 
 @dataclass
-class GramPerLiter:
+class GramPerLiter(HasUnit):
     unit: Optional[str] = "g/L"
 
 
 @dataclass
-class UnitPerLiter:
+class UnitPerLiter(HasUnit):
     unit: Optional[str] = "U/L"
 
 
 @dataclass
-class MillimeterOfMercury:
+class MillimeterOfMercury(HasUnit):
     unit: Optional[str] = "mmHg"
 
 
 @dataclass
-class OpticalDensity:
+class OpticalDensity(HasUnit):
     unit: Optional[str] = "OD"
 
 
 @dataclass
-class PH:
+class PH(HasUnit):
     unit: Optional[str] = "pH"
 
 
 @dataclass
-class MilliOsmolesPerKilogram:
+class MilliOsmolesPerKilogram(HasUnit):
     unit: Optional[str] = "mosm/kg"
 
 
 @dataclass
-class MillimolePerLiter:
+class MillimolePerLiter(HasUnit):
     unit: Optional[str] = "mmol/L"
 
 
 @dataclass
-class MilliAbsorbanceUnit:
+class MilliAbsorbanceUnit(HasUnit):
     unit: Optional[str] = "mAU"
 
 
 @dataclass
-class RelativeFluorescenceUnit:
+class RelativeFluorescenceUnit(HasUnit):
     unit: Optional[str] = "RFU"
 
 
 @dataclass
-class RelativeLightUnit:
+class RelativeLightUnit(HasUnit):
     unit: Optional[str] = "RLU"
 
 
 @dataclass
-class SquareCentimetersPerGram:
+class SquareCentimetersPerGram(HasUnit):
     unit: Optional[str] = "cm^2/g"
