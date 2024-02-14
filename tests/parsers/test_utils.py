5362299e0dc2cf4a7f64eac26434152f04fed83e
Brian Worth <brian@benchling.com>
2024-02-14 18:35:53 -0500

    better

--- tests/parsers/test_utils.py
+++ tests/parsers/test_utils.py
@@ -83,11 +83,11 @@ def _validate_contents(
     allotrope_dict: DictType,
     expected_file: str,
     identifiers_to_exclude: Optional[list[str]],
-    write_actual_to_expected_on_fail: bool,  # noqa: FBT001
 ) -> None:
     """Use the newly created allotrope_dict to validate the contents inside expected_file."""
     with open(expected_file) as f:
         expected_dict = json.load(f)
+
     expected_replaced = _replace_asm_converter_name_and_version(expected_dict)
     identifiers_to_exclude = identifiers_to_exclude or [
         CALCULATED_DATA_IDENTIFIER,
@@ -106,12 +106,7 @@ def _validate_contents(
         ignore_type_in_groups=[(float, np.float64), (int, np.int64)],
     )
 
-    try:
-        assert not ddiff
-    except AssertionError:
-        if write_actual_to_expected_on_fail:
-            _write_actual_to_expected(allotrope_dict, expected_file)
-        raise
+    assert not ddiff
 
 
 def generate_allotrope_and_validate(
@@ -120,16 +115,21 @@ def generate_allotrope_and_validate(
     schema_relative_path: str,
     expected_output_file: str,
     identifiers_to_exclude: Optional[list[str]] = None,
-    write_actual_to_expected_on_fail: bool = False,  # noqa: FBT001, FBT002
+    write_actual_to_expected_on_fail: bool = True,  # noqa: FBT001, FBT002
 ) -> None:
     allotrope_dict = from_file(test_file, vendor)
     _validate_schema(allotrope_dict, schema_relative_path)
-    _validate_contents(
-        allotrope_dict,
-        expected_output_file,
-        identifiers_to_exclude,
-        write_actual_to_expected_on_fail,
-    )
+
+    try:
+        _validate_contents(
+            allotrope_dict,
+            expected_output_file,
+            identifiers_to_exclude,
+        )
+    except Exception:
+        if write_actual_to_expected_on_fail:
+            _write_actual_to_expected(allotrope_dict, expected_output_file)
+        raise
 
     # Ensure that tests fail if the param is set to True. We never want to commit with a True value.
     assert not write_actual_to_expected_on_fail
