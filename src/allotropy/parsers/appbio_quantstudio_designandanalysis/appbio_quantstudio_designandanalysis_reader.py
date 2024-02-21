c5ca3a33d77bf1f5bd6a4f3c46519b1165b5ed95
Alejandro Salgado <alejando.salgado@benchling.com>
2024-02-21 09:59:43 -0500

    redefine reader metadata as header in design quantstudio

--- src/allotropy/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_reader.py
+++ src/allotropy/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_reader.py
@@ -9,8 +9,21 @@ from allotropy.types import IOType
 class DesignAndAnalysisReader:
     def __init__(self, contents: IOType) -> None:
         self.contents = contents
+        self.header = self._read_header()
         self.data = self._read_data()
-        self.metadata = self._read_metadata()
+
+    def _read_header(self) -> pd.Series[str]:
+        raw_header = pd.read_excel(
+            self.contents,
+            nrows=24,
+            usecols="A:B",
+            names=["index", "values"],
+            header=None,
+        ).replace(np.nan, None)
+
+        header = pd.Series(raw_header["values"].values, index=raw_header["index"])
+        header.index = header.index.str.strip()
+        return header
 
     def _read_data(self) -> dict[str, pd.DataFrame]:
         file_data = pd.read_excel(
@@ -21,16 +34,3 @@ class DesignAndAnalysisReader:
         for sheet in file_data.values():
             sheet.replace(np.nan, None, inplace=True)
         return file_data
-
-    def _read_metadata(self) -> pd.Series[str]:
-        raw_metadata = pd.read_excel(
-            self.contents,
-            nrows=24,
-            usecols="A:B",
-            names=["index", "values"],
-            header=None,
-        ).replace(np.nan, None)
-
-        metadata = pd.Series(raw_metadata["values"].values, index=raw_metadata["index"])
-        metadata.index = metadata.index.str.strip()
-        return metadata
