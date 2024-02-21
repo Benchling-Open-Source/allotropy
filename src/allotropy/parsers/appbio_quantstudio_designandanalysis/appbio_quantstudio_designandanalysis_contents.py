64798668d81d91cb54c4df0066b869ffd89c7176
Alejandro Salgado <alejando.salgado@benchling.com>
2024-02-21 10:56:31 -0500

    add methods to get sheet from contents class in design quantstudio

--- src/allotropy/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_contents.py
+++ src/allotropy/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_contents.py
@@ -1,8 +1,14 @@
 from __future__ import annotations
 
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 
+from allotropy.parsers.utils.values import (
+    assert_not_empty_df,
+    assert_not_none,
+)
 from allotropy.types import IOType
 
 
@@ -34,3 +40,22 @@ class DesignQuantstudioContents:
         for sheet in file_data.values():
             sheet.replace(np.nan, None, inplace=True)
         return file_data
+
+    def get_sheet_or_none(self, sheet_name: str) -> Optional[pd.DataFrame]:
+        return self.data.get(sheet_name)
+
+    def get_non_empty_sheet_or_none(self, sheet_name: str) -> Optional[pd.DataFrame]:
+        sheet = self.get_sheet_or_none(sheet_name)
+        return None if sheet is None or sheet.empty else sheet
+
+    def get_sheet(self, sheet_name: str) -> pd.DataFrame:
+        return assert_not_none(
+            self.get_sheet_or_none(sheet_name),
+            msg=f"Unable to find '{sheet_name}' sheet in file.",
+        )
+
+    def get_non_empty_sheet(self, sheet_name: str) -> pd.DataFrame:
+        return assert_not_empty_df(
+            self.get_sheet(sheet_name),
+            msg=f"sheet '{sheet_name}' is empty.",
+        )
