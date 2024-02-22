d96ea0b8e757d502e1574b39bdef2b11592f8609
Alejandro Salgado <alejando.salgado@benchling.com>
2024-02-22 11:07:45 -0500

    fix structure test for design quantstudio

--- tests/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_structure_test.py
+++ tests/parsers/appbio_quantstudio_designandanalysis/appbio_quantstudio_designandanalysis_structure_test.py
@@ -6,21 +6,11 @@ import pandas as pd
 import pytest
 
 from allotropy.exceptions import AllotropeConversionError
-from allotropy.parsers.appbio_quantstudio_designandanalysis.appbio_quantstudio_designandanalysis_contents import (
-    DesignQuantstudioContents,
-)
 from allotropy.parsers.appbio_quantstudio_designandanalysis.appbio_quantstudio_designandanalysis_structure import (
     Data,
     Header,
     Result,
     WellItem,
-)
-from tests.parsers.appbio_quantstudio_designandanalysis.appbio_quantstudio_designandanalysis_data import (
-    get_broken_calc_doc_data,
-    get_data,
-    get_data2,
-    get_genotyping_data,
-    get_rel_std_curve_data,
 )
 
 
@@ -31,13 +21,13 @@ def rm_uuid(data: Data) -> Data:
     return data
 
 
-@pytest.mark.short
 def test_header_builder_returns_header_instance() -> None:
     header_contents = get_raw_header_contents()
     assert isinstance(Header.create(header_contents), Header)
 
 
 def test_header_builder() -> None:
+    measurement_time = "2010-10-01 01:44:54 AM EDT"
     device_identifier = "device1"
     model_number = "123"
     device_serial_number = "1234"
@@ -47,9 +37,8 @@ def test_header_builder() -> None:
     experimental_data_identifier = "data Identifier"
 
     header_contents = get_raw_header_contents(
-        measurement_time="2010-10-01 01:44:54 AM EDT",
+        measurement_time=measurement_time,
         plate_well_count="96 plates",
-        experiment_type="Genotyping",
         device_identifier=device_identifier,
         model_number=model_number,
         device_serial_number=device_serial_number,
@@ -57,10 +46,12 @@ def test_header_builder() -> None:
         pcr_detection_chemistry=pcr_detection_chemistry,
         passive_reference_dye_setting=passive_reference_dye_setting,
         experimental_data_identifier=experimental_data_identifier,
+        pcr_stage_number="Stage 2 Step 2",
+        software_name_and_version="Design & Analysis Software v2.7.0",
     )
 
     assert Header.create(header_contents) == Header(
-        measurement_time="2010-10-01 01:44:54 AM EDT",
+        measurement_time=measurement_time,
         plate_well_count=96,
         device_identifier=device_identifier,
         model_number=model_number,
@@ -71,9 +62,9 @@ def test_header_builder() -> None:
         barcode=None,
         analyst=None,
         experimental_data_identifier=experimental_data_identifier,
-        pcr_stage_number=1,
-        software_name="",
-        software_version="",
+        pcr_stage_number=2,
+        software_name="Design & Analysis Software",
+        software_version="2.7.0",
     )
 
 
@@ -85,11 +76,9 @@ def test_header_builder() -> None:
             "measurement_method_identifier",
             "Expected non-null value for Quantification Cycle Method.",
         ),
-        ("pcr_detection_chemistry", "Expected non-null value for Chemistry."),
         ("plate_well_count", "Expected non-null value for Block Type."),
     ],
 )
-@pytest.mark.short
 def test_header_builder_required_parameter_none_then_raise(
     parameter: str, expected_error: str
 ) -> None:
@@ -99,7 +88,6 @@ def test_header_builder_required_parameter_none_then_raise(
         Header.create(header_contents)
 
 
-@pytest.mark.short
 def test_header_builder_invalid_plate_well_count() -> None:
     header_contents = get_raw_header_contents(plate_well_count="0 plates")
 
@@ -107,103 +95,108 @@ def test_header_builder_invalid_plate_well_count() -> None:
         Header.create(header_contents)
 
 
-@pytest.mark.short
 def test_header_builder_no_header_then_raise() -> None:
     with pytest.raises(AllotropeConversionError):
         Header.create(pd.Series())
 
 
-@pytest.mark.short
 def test_results_builder() -> None:
     data = pd.DataFrame(
         {
             "Well": [1],
-            "SNP Assay Name": ["CYP19_2"],
-            "Allele1 Automatic Ct Threshold": [True],
-            "Allele1 Ct Threshold": [0.219],
-            "Allele2 Ct Threshold": [9999],
-            "Allele1 Ct": ["Undetermined"],
+            "Well Position": ["A1"],
+            "Omit": [False],
+            "Sample": ["Unk_5K"],
+            "Target": ["RNaseP"],
+            "Task": ["UNKNOWN"],
+            "Reporter": ["FAM"],
+            "Quencher": ["NFQ-MGB"],
+            "Amp Status": ["AMP"],
+            "Amp Score": [1.280378],
+            "Curve Quality": [None],
+            "Result Quality Issues": [None],
+            "Cq": [28.037617],
+            "Cq Confidence": [0.978006],
+            "Cq Mean": [28.342053],
+            "Cq SD": [0.172629],
+            "Auto Threshold": [False],
+            "Threshold": [0.1],
+            "Auto Baseline": [True],
+            "Baseline Start": [3],
+            "Baseline End": [22],
         }
     )
     well_item = WellItem(
-        uuid="be566c58-41f3-40f8-900b-ef1dff20d264",
+        uuid="2b557369-c679-4a6f-b2cb-6b899ef9ab9d",
         identifier=1,
+        target_dna_description="RNaseP",
+        sample_identifier="Unk_5K",
+        reporter_dye_setting="FAM",
         position="A1",
-        target_dna_description="CYP19_2-Allele 1",
-        sample_identifier="NTC",
         well_location_identifier="A1",
-        reporter_dye_setting="SYBR",
-        quencher_dye_setting=None,
-        sample_role_type="PC_ALLELE_1",
+        quencher_dye_setting="NFQ-MGB",
+        sample_role_type="UNKNOWN",
     )
     result = Result.create(data, well_item)
     assert isinstance(result, Result)
-    assert result.cycle_threshold_value_setting == 0.219
-    assert result.cycle_threshold_result is None
-    assert result.automatic_cycle_threshold_enabled_setting is True
-
-
-@pytest.mark.short
-@pytest.mark.parametrize(
-    "test_filepath,expected_data",
-    [
-        (
-            "tests/parsers/appbio_quantstudio/testdata/appbio_quantstudio_test01.txt",
-            get_data(),
-        ),
-        (
-            "tests/parsers/appbio_quantstudio/testdata/appbio_quantstudio_test02.txt",
-            get_data2(),
-        ),
-        (
-            "tests/parsers/appbio_quantstudio/testdata/appbio_quantstudio_test03.txt",
-            get_genotyping_data(),
-        ),
-        (
-            "tests/parsers/appbio_quantstudio/testdata/appbio_quantstudio_test04.txt",
-            get_rel_std_curve_data(),
-        ),
-        (
-            "tests/parsers/appbio_quantstudio/testdata/appbio_quantstudio_test05.txt",
-            get_broken_calc_doc_data(),
-        ),
-    ],
-)
-def test_data_builder(test_filepath: str, expected_data: Data) -> None:
-    with open(test_filepath, "rb") as raw_contents:
-        reader = DesignQuantstudioContents(raw_contents)
-    assert rm_uuid(Data.create(reader)) == rm_uuid(expected_data)
+    assert result.cycle_threshold_value_setting == 0.1
+    assert result.cycle_threshold_result == 28.037617
+    assert result.automatic_cycle_threshold_enabled_setting is False
+    assert result.automatic_baseline_determination_enabled_setting is True
+    assert result.normalized_reporter_result is None
+    assert result.baseline_corrected_reporter_result is None
+    assert result.genotyping_determination_result is None
+    assert result.genotyping_determination_method_setting == 0.1
+    assert result.quantity is None
+    assert result.quantity_mean is None
+    assert result.quantity_sd is None
+    assert result.ct_mean == 28.342053
+    assert result.ct_sd == 0.172629
+    assert result.delta_ct_mean is None
+    assert result.delta_ct_se is None
+    assert result.delta_delta_ct is None
+    assert result.rq is None
+    assert result.rq_min is None
+    assert result.rq_max is None
+    assert result.rn_mean is None
+    assert result.rn_sd is None
+    assert result.y_intercept is None
+    assert result.r_squared is None
+    assert result.slope is None
+    assert result.efficiency is None
 
 
 def get_raw_header_contents(
     measurement_time: Optional[str] = "2010-10-01 01:44:54 AM EDT",
-    plate_well_count: Optional[str] = "96-Well Block (0.2mL)",
-    experiment_type: Optional[str] = "Presence/Absence",
-    device_identifier: Optional[str] = "278880034",
+    plate_well_count: Optional[str] = "96-Well 0.2-mL Block",
+    device_identifier: Optional[str] = "QS1-Eng2",
     model_number: Optional[str] = "QuantStudio(TM) 6 Flex System",
     device_serial_number: Optional[str] = "278880034",
     measurement_method_identifier: Optional[str] = "Ct",
     pcr_detection_chemistry: Optional[str] = "TAQMAN",
     passive_reference_dye_setting: Optional[str] = "ROX",
-    barcode: Optional[str] = "NA",
-    analyst: Optional[str] = "NA",
+    barcode: Optional[str] = None,
+    analyst: Optional[str] = None,
     experimental_data_identifier: Optional[
         str
     ] = "QuantStudio 96-Well Presence-Absence Example",
+    pcr_stage_number: Optional[str] = "Stage 2 Step 2",
+    software_name_and_version: Optional[str] = "Design & Analysis Software v2.7.0",
 ) -> pd.Series[str]:
     return pd.Series(
         {
-            "Experiment Run End Time": measurement_time,
+            "Run End Data/Time": measurement_time,
             "Block Type": plate_well_count,
-            "Experiment Type ": experiment_type,
             "Instrument Name": device_identifier,
             "Instrument Type": model_number,
             "Instrument Serial Number": device_serial_number,
             "Quantification Cycle Method": measurement_method_identifier,
             "Chemistry": pcr_detection_chemistry,
             "Passive Reference": passive_reference_dye_setting,
-            "Experiment Barcode": barcode,
-            "Experiment User Name": analyst,
+            "Barcode": barcode,
+            "Operator": analyst,
             "Experiment Name": experimental_data_identifier,
+            "PCR Stage/Step Number": pcr_stage_number,
+            "Software Name and Version": software_name_and_version,
         }
     )
