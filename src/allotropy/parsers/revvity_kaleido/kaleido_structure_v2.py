734b9d466903f16b132ae6d1a6d8e554bbd539f4
Alejandro Salgado <alejando.salgado@benchling.com>
2024-03-15 09:40:05 -0500

    add results dataclass to kaleido v2 structure

--- src/allotropy/parsers/revvity_kaleido/kaleido_structure_v2.py
+++ src/allotropy/parsers/revvity_kaleido/kaleido_structure_v2.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import pandas as pd
+
 from allotropy.parsers.lines_reader import CsvReader
 from allotropy.parsers.utils.values import (
     assert_not_none,
@@ -22,11 +24,36 @@ class BackgroundInfo:
 
 
 @dataclass(frozen=True)
+class Results:
+    barcode: str
+    results: pd.DataFrame
+
+    @staticmethod
+    def create(reader: CsvReader) -> Results:
+        barcode = assert_not_none(
+            reader.drop_until_inclusive("^Barcode"),
+            msg="Unable to find background information.",
+        )
+
+        results = assert_not_none(
+            reader.pop_csv_block_as_df(header=0, index_col=0),
+            msg="Unable to find results table.",
+        )
+
+        return Results(
+            barcode=barcode,
+            results=results,
+        )
+
+
+@dataclass(frozen=True)
 class DataV2:
     background_info: BackgroundInfo
+    results: Results
 
     @staticmethod
     def create(reader: CsvReader) -> DataV2:
         return DataV2(
             background_info=BackgroundInfo.create(reader),
+            results=Results.create(reader),
         )
