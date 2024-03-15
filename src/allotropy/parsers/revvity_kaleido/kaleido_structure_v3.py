220e6e65ac5875f08470677b01d52f2f2a1551ea
Alejandro Salgado <alejando.salgado@benchling.com>
2024-03-15 16:29:13 -0500

    add initial dataclasses to kaleido v3 structure

--- src/allotropy/parsers/revvity_kaleido/kaleido_structure_v3.py
+++ src/allotropy/parsers/revvity_kaleido/kaleido_structure_v3.py
@@ -2,11 +2,80 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+import pandas as pd
+
 from allotropy.parsers.lines_reader import CsvReader
+from allotropy.parsers.utils.values import assert_not_none
+
+
+@dataclass(frozen=True)
+class EnsightResults:
+    elements: dict[str, str]
+
+    @staticmethod
+    def create(reader: CsvReader) -> EnsightResults:
+        assert_not_none(
+            reader.pop_if_match("^EnSight Results from"),
+            msg="Unable to find EnSight section.",
+        )
+
+        elements = {}
+        for raw_line in reader.pop_until("^Result for"):
+            if raw_line == "":
+                continue
+
+            key, value, *_ = raw_line.split(",")
+            elements[key.rstrip(":")] = value
+
+        return EnsightResults(elements)
+
+
+@dataclass(frozen=True)
+class BackgroundInfo:
+    info: str
+
+    @staticmethod
+    def create(reader: CsvReader) -> BackgroundInfo:
+        line = assert_not_none(
+            reader.drop_until_inclusive("^Result for"),
+            msg="Unable to find background information.",
+        )
+        return BackgroundInfo(line)
+
+
+@dataclass(frozen=True)
+class Results:
+    metadata: pd.DataFrame
+    results: pd.DataFrame
+
+    @staticmethod
+    def create(reader: CsvReader) -> Results:
+        metadata = assert_not_none(
+            reader.pop_csv_block_as_df(header=0),
+            msg="Unable to find Result barcode information.",
+        )
+
+        results = assert_not_none(
+            reader.pop_csv_block_as_df(header=0, index_col=0),
+            msg="Unable to find results table.",
+        )
+
+        return Results(
+            metadata=metadata,
+            results=results,
+        )
 
 
 @dataclass(frozen=True)
 class DataV3:
+    ensight_results: EnsightResults
+    background_info: BackgroundInfo
+    results: Results
+
     @staticmethod
-    def create(_: CsvReader) -> DataV3:
-        return DataV3()
+    def create(reader: CsvReader) -> DataV3:
+        return DataV3(
+            ensight_results=EnsightResults.create(reader),
+            background_info=BackgroundInfo.create(reader),
+            results=Results.create(reader),
+        )
