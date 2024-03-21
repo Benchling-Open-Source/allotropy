a484e2ecd14a5e9eb42f825c47b52da5d7eb47ec
Alejandro Salgado <alejando.salgado@benchling.com>
2024-03-21 11:49:42 -0500

    simplify sample document construction in kaleido adapter

--- src/allotropy/parsers/revvity_kaleido/kaleido_parser.py
+++ src/allotropy/parsers/revvity_kaleido/kaleido_parser.py
@@ -53,27 +53,32 @@ class MeasurementParser(ABC):
     ) -> MeasurementItem:
         pass
 
-
-class FluorescenceMeasurementParser(MeasurementParser):
-    def parse(
+    def get_sample_document(
         self, data: Union[DataV2, DataV3], well_position: WellPosition
-    ) -> MeasurementItem:
+    ) -> SampleDocument:
         well_plate_identifier = data.get_well_plate_identifier()
         sample_identifier = (
             data.get_platemap_well_value(well_position)
             or f"{well_plate_identifier}_{well_position}"
         )
+        return SampleDocument(
+            sample_identifier=sample_identifier,
+            location_identifier=str(well_position),
+            well_plate_identifier=well_plate_identifier,
+            sample_role_type=data.get_sample_role_type(well_position),
+        )
+
+
+class FluorescenceMeasurementParser(MeasurementParser):
+    def parse(
+        self, data: Union[DataV2, DataV3], well_position: WellPosition
+    ) -> MeasurementItem:
         return FluorescencePointDetectionMeasurementDocumentItems(
             measurement_identifier=random_uuid_str(),
             fluorescence=TQuantityValueRelativeFluorescenceUnit(
                 value=data.get_well_value(well_position)
             ),
-            sample_document=SampleDocument(
-                sample_identifier=sample_identifier,
-                location_identifier=str(well_position),
-                well_plate_identifier=well_plate_identifier,
-                sample_role_type=data.get_sample_role_type(well_position),
-            ),
+            sample_document=self.get_sample_document(data, well_position),
             device_control_aggregate_document=FluorescencePointDetectionDeviceControlAggregateDocument(
                 device_control_document=[
                     FluorescencePointDetectionDeviceControlDocumentItem(
@@ -88,22 +93,12 @@ class AbsorbanceMeasurementParser(MeasurementParser):
     def parse(
         self, data: Union[DataV2, DataV3], well_position: WellPosition
     ) -> MeasurementItem:
-        well_plate_identifier = data.get_well_plate_identifier()
-        sample_identifier = (
-            data.get_platemap_well_value(well_position)
-            or f"{well_plate_identifier}_{well_position}"
-        )
         return UltravioletAbsorbancePointDetectionMeasurementDocumentItems(
             measurement_identifier=random_uuid_str(),
             absorbance=TQuantityValueMilliAbsorbanceUnit(
                 value=data.get_well_value(well_position)
             ),
-            sample_document=SampleDocument(
-                sample_identifier=sample_identifier,
-                location_identifier=str(well_position),
-                well_plate_identifier=well_plate_identifier,
-                sample_role_type=data.get_sample_role_type(well_position),
-            ),
+            sample_document=self.get_sample_document(data, well_position),
             device_control_aggregate_document=UltravioletAbsorbancePointDetectionDeviceControlAggregateDocument(
                 device_control_document=[
                     UltravioletAbsorbancePointDetectionDeviceControlDocumentItem(
@@ -118,22 +113,12 @@ class LuminescenceMeasurementParser(MeasurementParser):
     def parse(
         self, data: Union[DataV2, DataV3], well_position: WellPosition
     ) -> MeasurementItem:
-        well_plate_identifier = data.get_well_plate_identifier()
-        sample_identifier = (
-            data.get_platemap_well_value(well_position)
-            or f"{well_plate_identifier}_{well_position}"
-        )
         return LuminescencePointDetectionMeasurementDocumentItems(
             measurement_identifier=random_uuid_str(),
             luminescence=TQuantityValueRelativeLightUnit(
                 value=data.get_well_value(well_position)
             ),
-            sample_document=SampleDocument(
-                sample_identifier=sample_identifier,
-                location_identifier=str(well_position),
-                well_plate_identifier=well_plate_identifier,
-                sample_role_type=data.get_sample_role_type(well_position),
-            ),
+            sample_document=self.get_sample_document(data, well_position),
             device_control_aggregate_document=LuminescencePointDetectionDeviceControlAggregateDocument(
                 device_control_document=[
                     LuminescencePointDetectionDeviceControlDocumentItem(
