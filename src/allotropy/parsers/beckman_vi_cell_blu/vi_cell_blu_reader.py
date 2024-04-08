129fce0381d6290e50e740268fbac1b78acb6cdd
Nathan Stender <nathan.stender@benchling.com>
2024-04-08 13:52:19 -0700

    fix: Revert "add encoding inference to Beckman Vi Cell Blu adapter" (#368)
    Revert https://github.com/Benchling-Open-Source/allotropy/pull/365, it
    is causing unexpected behavior (dropping some fields from ASM output) in
    other environments.

--- src/allotropy/parsers/beckman_vi_cell_blu/vi_cell_blu_reader.py
+++ src/allotropy/parsers/beckman_vi_cell_blu/vi_cell_blu_reader.py
@@ -1,16 +1,10 @@
 import pandas as pd
 
-from allotropy.named_file_contents import NamedFileContents
-from allotropy.parsers.lines_reader import CsvReader, read_to_lines
-from allotropy.parsers.utils.values import assert_not_none
+from allotropy.allotrope.pandas_util import read_csv
+from allotropy.types import IOType
 
 
 class ViCellBluReader:
     @classmethod
-    def read(cls, named_file_contents: NamedFileContents) -> pd.DataFrame:
-        lines = read_to_lines(named_file_contents)
-        reader = CsvReader(lines)
-        return assert_not_none(
-            reader.pop_csv_block_as_df(index_col=False, header=0),
-            msg=f"Unable to read csv file '{named_file_contents.original_file_name}'.",
-        )
+    def read(cls, contents: IOType) -> pd.DataFrame:
+        return read_csv(contents, index_col=False)
