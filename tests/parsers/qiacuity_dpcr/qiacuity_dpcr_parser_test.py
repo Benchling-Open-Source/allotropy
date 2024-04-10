46d65e65afd878f5178fb21392fb68a9e6cfe011
Nathan Stender <nathan.stender@benchling.com>
2024-04-10 11:18:47 -0700

    feat: Move testing util to allotropy.testing (#371)
    This will allow users of library to reusing testing utils

--- tests/parsers/qiacuity_dpcr/qiacuity_dpcr_parser_test.py
+++ tests/parsers/qiacuity_dpcr/qiacuity_dpcr_parser_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from allotropy.parser_factory import Vendor
-from tests.parsers.test_utils import from_file, validate_contents
+from allotropy.testing.utils import from_file, validate_contents
 
 output_files = ("qiacuity_dpcr_example01.csv", "qiacuity_dpcr_example02.csv")
 
