f5b266d534209c5ba5d275f5b228717d95f12669
Sebastian Lopez <sebastian.cardona@benchling.com>
2024-04-22 12:34:06 -0500

    clean unused file

--- src/allotropy/parsers/agilent_gen5/constants.py
+++ src/allotropy/parsers/agilent_gen5/constants.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from typing import Optional
 
 MULTIPLATE_FILE_ERROR = "Only a single plate per file can be processed at this time. Please refer to Gen5 documentation for how to generate single plate exports from multi-plate experiments"
 NO_PLATE_DATA_ERROR = "No plate data found in file."
@@ -48,14 +47,3 @@ class ReadType(str, Enum):
     KINETIC = "Kinetic"
     AREASCAN = "Area Scan"
     SPECTRAL = "Spectral"
-
-
-READTYPE_TO_DIMENSIONS: dict[ReadType, list[tuple[str, str, Optional[str]]]] = {
-    ReadType.ENDPOINT: [("int", "wavelength", "nm")],
-    ReadType.KINETIC: [("double", "time", "s")],
-    ReadType.AREASCAN: [
-        ("int", "x", None),
-        ("int", "y", None),
-    ],
-    ReadType.SPECTRAL: [("int", "wavelength", "nm")],
-}
