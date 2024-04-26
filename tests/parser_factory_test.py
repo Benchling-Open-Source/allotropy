c83c4c479adecc0d91dfd0c9316cc417970e3862
Stephen Worlow <stephen.worlow@benchling.com>
2024-04-26 16:22:14 -0400

    Update

--- tests/parser_factory_test.py
+++ tests/parser_factory_test.py
@@ -3,4 +3,4 @@ from allotropy.parser_factory import Vendor
 
 def test_vendor_get_display_name() -> None:
     vendor = Vendor.APPBIO_QUANTSTUDIO
-    assert vendor.get_display_name() == "AppBio QuantStudio"
+    assert vendor.get_display_name() == "AppBio QuantStudio RT-PCR"
