0dd593690d44998e837163dd5eb0acd1523ecdb9
alejandro-salgado <alejando.salgado@benchling.com>
2024-05-07 11:36:54 -0500

    simplify AppBio Quantstudio views with defaultdict (#403)
    - simplify AppBio Quantstudio views with defaultdict

--- src/allotropy/parsers/appbio_quantstudio/appbio_quantstudio_views.py
+++ src/allotropy/parsers/appbio_quantstudio/appbio_quantstudio_views.py
@@ -1,33 +1,29 @@
+from collections import defaultdict
+
 from allotropy.parsers.appbio_quantstudio.appbio_quantstudio_structure import WellItem
 from allotropy.parsers.appbio_quantstudio.views import View
 
 
 class SampleView(View[WellItem]):
     def sort_elements(self, well_items: list[WellItem]) -> dict[str, list[WellItem]]:
-        items: dict[str, list[WellItem]] = {}
+        items = defaultdict(list)
         for well_item in well_items:
-            if well_item.sample_identifier not in items:
-                items[well_item.sample_identifier] = []
             items[well_item.sample_identifier].append(well_item)
-        return items
+        return dict(items)
 
 
 class TargetView(View[WellItem]):
     def sort_elements(self, well_items: list[WellItem]) -> dict[str, list[WellItem]]:
-        items: dict[str, list[WellItem]] = {}
+        items = defaultdict(list)
         for well_item in well_items:
-            if well_item.target_dna_description not in items:
-                items[well_item.target_dna_description] = []
             items[well_item.target_dna_description].append(well_item)
-        return items
+        return dict(items)
 
 
 class TargetRoleView(View[WellItem]):
     def sort_elements(self, well_items: list[WellItem]) -> dict[str, list[WellItem]]:
-        items: dict[str, list[WellItem]] = {}
+        items = defaultdict(list)
         for well_item in well_items:
             if well_item.sample_role_type == "STANDARD":
-                if well_item.target_dna_description not in items:
-                    items[well_item.target_dna_description] = []
                 items[well_item.target_dna_description].append(well_item)
-        return items
+        return dict(items)
