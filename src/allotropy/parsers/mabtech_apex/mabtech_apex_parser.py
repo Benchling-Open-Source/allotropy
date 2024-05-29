92f2e478b29d4b39006328d0a536ef915827b01d
james-leinas <157071641+james-leinas@users.noreply.github.com>
2024-05-29 12:19:20 -0700

    Fix parser issues with linter
    Signed-off-by: james-leinas <157071641+james-leinas@users.noreply.github.com>

--- src/allotropy/parsers/mabtech_apex/mabtech_apex_parser.py
+++ src/allotropy/parsers/mabtech_apex/mabtech_apex_parser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from allotropy.allotrope.models.plate_reader_benchling_2023_09_plate_reader import (
     ContainerType,
     DataSystemDocument,
@@ -14,7 +15,7 @@ from allotropy.allotrope.models.plate_reader_benchling_2023_09_plate_reader impo
     PlateReaderDocumentItem,
     ProcessedDataAggregateDocument,
     ProcessedDataDocumentItem,
-    SampleDocument
+    SampleDocument,
 )
 from allotropy.allotrope.models.shared.definitions.custom import (
     TQuantityValueMilliSecond,
@@ -23,20 +24,17 @@ from allotropy.allotrope.models.shared.definitions.custom import (
 )
 from allotropy.constants import ASM_CONVERTER_NAME, ASM_CONVERTER_VERSION
 from allotropy.named_file_contents import NamedFileContents
+from allotropy.parsers.mabtech_apex.mabtech_apex_contents import MabTechApexContents
 from allotropy.parsers.mabtech_apex.mabtech_apex_structure import (
     PlateInformation,
     Well,
-    WellList
-)
-from allotropy.parsers.mabtech_apex.mabtech_apex_contents import (
-    MabTechApexContents
+    WellList,
 )
 from allotropy.parsers.utils.uuids import random_uuid_str
 from allotropy.parsers.vendor_parser import VendorParser
 
 
 class MabTechApexParser(VendorParser):
-
     def to_allotrope(self, named_file_contents: NamedFileContents) -> Model:
         raw_contents = pd.read_excel(named_file_contents.contents, sheet_name=None)
         contents = MabTechApexContents(raw_contents)
@@ -44,13 +42,15 @@ class MabTechApexParser(VendorParser):
         wells = WellList.create(contents)
         return self._get_model(data, wells, named_file_contents.original_file_name)
 
-    def _get_model(self, data: PlateInformation, wells: WellList, file_name: str) -> Model:
+    def _get_model(
+        self, data: PlateInformation, wells: WellList, file_name: str
+    ) -> Model:
         return Model(
             plate_reader_aggregate_document=PlateReaderAggregateDocument(
                 device_system_document=DeviceSystemDocument(
                     device_identifier="N/A",
                     model_number=data.model_number,
-                    equipment_serial_number=data.equipment_serial_number
+                    equipment_serial_number=data.equipment_serial_number,
                 ),
                 data_system_document=DataSystemDocument(
                     file_name=file_name,
@@ -63,26 +63,29 @@ class MabTechApexParser(VendorParser):
                 plate_reader_document=[
                     PlateReaderDocumentItem(
                         analyst=data.analyst,
-                        measurement_aggregate_document=self.get_measurement_aggregate_document(well),
+                        measurement_aggregate_document=self.get_measurement_aggregate_document(
+                            well
+                        ),
                     )
                     for well in wells
-                ]
+                ],
             ),
             field_asm_manifest="http://purl.allotrope.org/manifests/plate-reader/BENCHLING/2023/09/plate-reader.manifest",
         )
 
-    def get_measurement_aggregate_document(self, well: Well) -> MeasurementAggregateDocument:
+    def get_measurement_aggregate_document(
+        self, well: Well
+    ) -> MeasurementAggregateDocument:
         return MeasurementAggregateDocument(
             container_type=ContainerType.well_plate,
             plate_well_count=TQuantityValueNumber(value=float(96)),
             measurement_time=self._get_date_time(well.measurement_time),
-            measurement_document=[
-                self.get_measurement_document_item(well)
-            ],
+            measurement_document=[self.get_measurement_document_item(well)],
         )
 
     def get_measurement_document_item(
-            self, well: Well) -> OpticalImagingMeasurementDocumentItems:
+        self, well: Well
+    ) -> OpticalImagingMeasurementDocumentItems:
         return OpticalImagingMeasurementDocumentItems(
             measurement_identifier=random_uuid_str(),
             sample_document=self.get_sample_document(well),
@@ -95,17 +98,19 @@ class MabTechApexParser(VendorParser):
                 processed_data_document=[
                     self.get_processed_data_document(well),
                 ]
-            )
+            ),
         )
 
     def get_sample_document(self, well: Well) -> SampleDocument:
         return SampleDocument(
             sample_identifier=well.sample_identifier,
             location_identifier=well.location_identifier,
-            well_plate_identifier=well.well_plate_identifier
+            well_plate_identifier=well.well_plate_identifier,
         )
 
-    def get_device_control_document_item(self, well: Well) -> OpticalImagingDeviceControlDocumentItem:
+    def get_device_control_document_item(
+        self, well: Well
+    ) -> OpticalImagingDeviceControlDocumentItem:
         return OpticalImagingDeviceControlDocumentItem(
             device_type="imager",
             detection_type="optical-imaging",
@@ -129,9 +134,11 @@ class MabTechApexParser(VendorParser):
                     ImageFeatureDocumentItem(
                         image_feature_identifier=random_uuid_str(),
                         image_feature_name=next(iter(image_feature.keys())),
-                        image_feature_result=TQuantityValueUnitless(value=image_feature.get(next(iter(image_feature.keys()))))
+                        image_feature_result=TQuantityValueUnitless(
+                            value=image_feature[next(iter(image_feature.keys()))]
+                        ),
                     )
                     for image_feature in well.image_features
                 ]
-            )
+            ),
         )
