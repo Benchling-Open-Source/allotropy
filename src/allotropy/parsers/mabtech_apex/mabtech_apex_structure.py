e3362a690e9a3685dad2ee70438f2f1d96a6e54d
james-leinas <157071641+james-leinas@users.noreply.github.com>
2024-05-29 12:19:51 -0700

    Fix structure issues with linter
    Signed-off-by: james-leinas <157071641+james-leinas@users.noreply.github.com>

--- src/allotropy/parsers/mabtech_apex/mabtech_apex_structure.py
+++ src/allotropy/parsers/mabtech_apex/mabtech_apex_structure.py
@@ -1,19 +1,26 @@
 from __future__ import annotations
+
 from collections.abc import Iterator
 from dataclasses import dataclass
 import re
+
 import pandas as pd
-from allotropy.parsers.mabtech_apex.mabtech_apex_contents import (
-    MabTechApexContents
-)
+
+from allotropy.allotrope.models.shared.definitions.definitions import InvalidJsonFloat
+from allotropy.parsers.mabtech_apex.mabtech_apex_contents import MabTechApexContents
 from allotropy.parsers.utils.values import (
     assert_not_none,
+    try_float_from_series_or_nan,
     try_float_from_series_or_none,
     try_str_from_series,
     try_str_from_series_or_none,
 )
 
-IMAGE_FEATURES = ["Spot Forming Units (SFU)", "Average Relative Spot Volume (RSV)", "Sum of Spot Volume (RSV)"]
+IMAGE_FEATURES = [
+    "Spot Forming Units (SFU)",
+    "Average Relative Spot Volume (RSV)",
+    "Sum of Spot Volume (RSV)",
+]
 
 
 @dataclass(frozen=True)
@@ -38,7 +45,9 @@ class PlateInformation:
         return PlateInformation(
             unc_path=try_str_from_series_or_none(plateinformation, key="Path:"),
             analyst=try_str_from_series_or_none(plateinformation, key="Saved By:"),
-            software_version=try_str_from_series_or_none(plateinformation, key="Software Version:"),
+            software_version=try_str_from_series_or_none(
+                plateinformation, key="Software Version:"
+            ),
             model_number=machine_id.group(1),
             equipment_serial_number=machine_id.group(2),
         )
@@ -52,10 +61,10 @@ class Well:
     well_plate_identifier: str | None
     exposure_duration_setting: float | None
     illumination_setting: float | None
-    image_features: list | None
+    image_features: list[dict[str, float | InvalidJsonFloat]]
 
     @staticmethod
-    def create(plate_data: pd.Series) -> Well | None:
+    def create(plate_data: pd.Series[str]) -> Well | None:
         # if Read Date is not present in file, return None, no measurement for given Well
         if try_str_from_series_or_none(plate_data, "Read Date") is None:
             return None
@@ -66,17 +75,23 @@ class Well:
 
         image_feature_list = []
         for feature in IMAGE_FEATURES:
-            image_dict = {str(feature): try_float_from_series_or_none(plate_data, feature)}
+            image_dict = {
+                str(feature): try_float_from_series_or_nan(plate_data, feature)
+            }
             image_feature_list.append(image_dict)
 
         return Well(
-            measurement_time=try_str_from_series_or_none(plate_data, "Read Date"),
+            measurement_time=try_str_from_series(plate_data, "Read Date"),
             location_identifier=location_id,
             well_plate_identifier=well_plate,
             sample_identifier=f"{well_plate}_{location_id}",
-            exposure_duration_setting=try_float_from_series_or_none(plate_data, "Exposure"),
-            illumination_setting=try_float_from_series_or_none(plate_data, "Preset Intensity"),
-            image_features=image_feature_list
+            exposure_duration_setting=try_float_from_series_or_none(
+                plate_data, "Exposure"
+            ),
+            illumination_setting=try_float_from_series_or_none(
+                plate_data, "Preset Intensity"
+            ),
+            image_features=image_feature_list,
         )
 
 
@@ -95,6 +110,4 @@ class WellList:
             well = Well.create(well_data)
             if well is not None:
                 well_list.append(well)
-        return WellList(
-            wells=well_list
-        )
+        return WellList(wells=well_list)
