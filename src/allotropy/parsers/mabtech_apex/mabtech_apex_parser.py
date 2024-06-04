3a9aadffc6906d57f60fd960715b0b8c0096a030
james-leinas <157071641+james-leinas@users.noreply.github.com>
2024-06-04 09:02:04 -0700

    Update parser to new schema folder structure
    Signed-off-by: james-leinas <157071641+james-leinas@users.noreply.github.com>

--- src/allotropy/parsers/mabtech_apex/mabtech_apex_parser.py
+++ src/allotropy/parsers/mabtech_apex/mabtech_apex_parser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from allotropy.allotrope.models.plate_reader_benchling_2023_09_plate_reader import (
+from allotropy.allotrope.models.plate_reader.benchling._2023._09.plater_reader import (
     ContainerType,
     DataSystemDocument,
     DeviceSystemDocument,
