47684e964334a6ffc6793d77b4719f8d475eacaf
Nathan Stender <nathan.stender@benchling.com>
2024-07-10 01:31:55 -0400

    agilent_gen5

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,19 @@ from pathlib import Path
 import pytest
 
 
+# ParserTest will ignore any files with "error" or "exclude" in their paths.
+def _is_valid_testcase(path: Path) -> bool:
+    if not path.is_file():
+        return False
+    if "error" in str(path) or "exclude" in str(path):
+        return False
+    return path.suffix.lower() != ".json"
+
+
 def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
     # Only parameterize test_file_path if class variable VENDOR is defined, signifying a ParserTest
     if "test_file_path" in metafunc.fixturenames and metafunc.cls.VENDOR:
         testdata_dir = Path(Path(inspect.getfile(metafunc.cls)).parent, "testdata")
-        paths = [path for path in testdata_dir.rglob("*") if path.suffix.lower() != ".json"]
-        metafunc.parametrize("test_file_path", paths)
+        paths = [path for path in testdata_dir.rglob("*") if _is_valid_testcase(path)]
+        ids = [str(path.relative_to(testdata_dir)) for path in paths]
+        metafunc.parametrize("test_file_path", paths, ids=ids)
