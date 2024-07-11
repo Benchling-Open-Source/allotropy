736ab18ac5ea6cdd512b0bee247810badcb7d9fd
Nathan Stender <nathan.stender@benchling.com>
2024-07-11 15:00:45 -0400

    Regenerate readme

--- scripts/update_readme.py
+++ scripts/update_readme.py
@@ -39,7 +39,7 @@ def update_readme() -> None:
                         ReleaseState.CANDIDATE_RELEASE,
                         ReleaseState.WORKING_DRAFT,
                     ]:
-                        f.write(f"### {release_state.value.title()}\n")
+                        f.write(f'### {release_state.value.replace("_", " ").title()}\n')
                         for display_name in sorted(
                             release_state_to_parser.get(release_state, [])
                         ):
