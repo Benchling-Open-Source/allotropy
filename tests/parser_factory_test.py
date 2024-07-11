2e6f0bfb73c028e45c672e402cf56210e7482244
Nathan Stender <nathan.stender@benchling.com>
2024-07-11 15:43:01 -0400

    Add hint to README test

--- tests/parser_factory_test.py
+++ tests/parser_factory_test.py
@@ -60,10 +60,10 @@ def test_vendors_in_readme() -> None:
 
     # Assert all vendors are in README
     for vendor in Vendor:
-        assert vendor.display_name in parsers[vendor.release_state]
+        assert vendor.display_name in parsers[vendor.release_state], f"Missing vendor in README: '{vendor.display_name}'. Hint: run 'hatch run scripts:update-readme'"
 
     # Assert not extra parsers in README
     assert (
         set.union(*parsers.values()) - {vendor.display_name for vendor in Vendor}
         == set()
-    )
+    ), f"Extra vendor in README: '{vendor.display_name}'. Hint: run 'hatch run scripts:update-readme'"
