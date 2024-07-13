81160c6a5bdbe18145fff3c1326fa7732efe9db3
Nathan Stender <nathan.stender@benchling.com>
2024-07-13 14:36:22 -0400

    xr done

--- src/allotropy/parsers/beckman_vi_cell_xr/vi_cell_xr_parser.py
+++ src/allotropy/parsers/beckman_vi_cell_xr/vi_cell_xr_parser.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
 
-from enum import Enum
 import tempfile
-from typing import Any
 import zipfile
-
-import pandas as pd
 
 from allotropy.allotrope.models.adm.cell_counting.benchling._2023._11.cell_counting import (
     CellCountingAggregateDocument,
@@ -30,41 +26,18 @@ from allotropy.allotrope.models.shared.definitions.custom import (
     TQuantityValueUnitless,
 )
 from allotropy.constants import ASM_CONVERTER_VERSION
-from allotropy.exceptions import AllotropeConversionError
 from allotropy.named_file_contents import NamedFileContents
-from allotropy.parsers.beckman_vi_cell_xr.constants import (
-    DATE_HEADER,
-    DEFAULT_ANALYST,
-    MODEL_NUMBER,
-    SOFTWARE_NAME,
-    XrVersion,
-)
 from allotropy.parsers.beckman_vi_cell_xr.vi_cell_xr_reader import ViCellXRReader
+from allotropy.parsers.beckman_vi_cell_xr.vi_cell_xr_structure import (
+    Data,
+    Metadata,
+    Row,
+)
 from allotropy.parsers.beckman_vi_cell_xr.vi_cell_xr_txt_reader import ViCellXRTXTReader
 from allotropy.parsers.release_state import ReleaseState
-from allotropy.parsers.utils.uuids import random_uuid_str
+from allotropy.parsers.utils.values import quantity_or_none
 from allotropy.parsers.vendor_parser import VendorParser
 from allotropy.types import IOType
-
-
-class SampleProperty(Enum):
-    DILUTION_FACTOR = ("Dilution factor", TQuantityValueUnitless)
-    TOTAL_CELLS_ML = ("Total cells/ml (x10^6)", TQuantityValueMillionCellsPerMilliliter)
-    TOTAL_CELLS = ("Total cells", TQuantityValueCell)
-    AVERAGE_DIAMETER = ("Avg. diam. (microns)", TQuantityValueMicrometer)
-    VIABLE_CELLS = ("Viable cells", TQuantityValueCell)
-    AVERAGE_CIRCULARITY = ("Avg. circ.", TQuantityValueUnitless)
-
-    def __init__(self, column_name: str, data_type: Any) -> None:
-        self.column_name: str = column_name
-        self.data_type: Any = data_type
-
-
-def get_property_from_sample(
-    sample: pd.Series[Any], sample_property: SampleProperty
-) -> Any:
-    value = sample.get(sample_property.column_name)
-    return sample_property.data_type(value=value) if value else None
 
 
 def remove_style_xml_file(contents: IOType) -> IOType:
@@ -113,76 +86,47 @@ class ViCellXRParser(VendorParser):
         else:
             reader = ViCellXRReader(contents)
 
+        data = Data.create(reader)
+
         return Model(
             field_asm_manifest="http://purl.allotrope.org/manifests/cell-counting/BENCHLING/2023/11/cell-counting.manifest",
             cell_counting_aggregate_document=CellCountingAggregateDocument(
                 device_system_document=DeviceSystemDocument(
-                    model_number=MODEL_NUMBER,
-                    equipment_serial_number=self._get_device_serial_number(
-                        reader.file_info
-                    ),
+                    model_number=data.metadata.model_number,
+                    equipment_serial_number=data.metadata.equipment_serial_number,
                 ),
                 data_system_document=DataSystemDocument(
                     file_name=filename,
-                    software_name=SOFTWARE_NAME,
-                    software_version=reader.file_version.value,
+                    software_name=data.metadata.software_name,
+                    software_version=data.metadata.software_version,
                     ASM_converter_name=self.get_asm_converter_name(),
                     ASM_converter_version=ASM_CONVERTER_VERSION,
                 ),
                 cell_counting_document=[
-                    self._get_cell_counting_document_item(sample, reader.file_version)
-                    for _, sample in reader.data.iterrows()
+                    self._get_cell_counting_document_item(row, data.metadata)
+                    for row in data.rows
                 ],
             ),
         )
 
-    def _get_device_serial_number(self, file_info: pd.Series[Any]) -> str | None:
-        serial = str(file_info["serial"])
-        try:
-            serial_number = serial[serial.rindex(":") + 1 :].strip()
-        except ValueError:
-            return None
-
-        return serial_number
-
     def _get_cell_counting_document_item(
-        self, sample: pd.Series[Any], file_version: XrVersion
+        self, row: Row, metadata: Metadata
     ) -> CellCountingDocumentItem:
-        required_fields_list = [
-            "Viability (%)",
-            "Viable cells/ml (x10^6)",
-        ]
-        # Required fields
-        try:
-            viability__cell_counter_ = TQuantityValuePercent(
-                value=sample["Viability (%)"]
-            )
-            viable_cell_density__cell_counter_ = (
-                TQuantityValueMillionCellsPerMilliliter(
-                    value=sample["Viable cells/ml (x10^6)"]
-                )
-            )
-        except KeyError as e:
-            error = f"Expected to find lines with all of these headers: {required_fields_list}."
-            raise AllotropeConversionError(error) from e
-
         return CellCountingDocumentItem(
-            analyst=DEFAULT_ANALYST,
+            analyst=metadata.analyst,
             measurement_aggregate_document=MeasurementAggregateDocument(
                 measurement_document=[
                     CellCountingDetectorMeasurementDocumentItem(
-                        measurement_identifier=random_uuid_str(),
-                        measurement_time=self._get_date_time(
-                            str(sample.get(DATE_HEADER[file_version]))
-                        ),
+                        measurement_identifier=row.measurement_identifier,
+                        measurement_time=self._get_date_time(row.timestamp),
                         sample_document=SampleDocument(
-                            sample_identifier=sample["Sample ID"]
+                            sample_identifier=row.sample_identifier
                         ),
                         device_control_aggregate_document=CellCountingDetectorDeviceControlAggregateDocument(
                             device_control_document=[
                                 DeviceControlDocumentItemModel(
-                                    device_type="brightfield imager (cell counter)",
-                                    detection_type="brightfield",
+                                    device_type=metadata.device_type,
+                                    detection_type=metadata.detection_type,
                                 )
                             ]
                         ),
@@ -190,28 +134,35 @@ class ViCellXRParser(VendorParser):
                             processed_data_document=[
                                 ProcessedDataDocumentItem(
                                     data_processing_document=DataProcessingDocument(
-                                        cell_type_processing_method=sample.get("Cell type"),  # type: ignore[arg-type]
-                                        cell_density_dilution_factor=get_property_from_sample(
-                                            sample,
-                                            SampleProperty.DILUTION_FACTOR,
+                                        cell_type_processing_method=row.cell_type_processing_method,
+                                        cell_density_dilution_factor=quantity_or_none(
+                                            TQuantityValueUnitless,
+                                            row.cell_density_dilution_factor,
                                         ),
                                     ),
-                                    viability__cell_counter_=viability__cell_counter_,
-                                    viable_cell_density__cell_counter_=viable_cell_density__cell_counter_,
-                                    total_cell_count=get_property_from_sample(
-                                        sample, SampleProperty.TOTAL_CELLS
+                                    viability__cell_counter_=TQuantityValuePercent(
+                                        value=row.viability
                                     ),
-                                    total_cell_density__cell_counter_=get_property_from_sample(
-                                        sample, SampleProperty.TOTAL_CELLS_ML
+                                    viable_cell_density__cell_counter_=TQuantityValueMillionCellsPerMilliliter(
+                                        value=row.viable_cell_density
                                     ),
-                                    average_total_cell_diameter=get_property_from_sample(
-                                        sample, SampleProperty.AVERAGE_DIAMETER
+                                    total_cell_count=quantity_or_none(
+                                        TQuantityValueCell, row.total_cell_count
                                     ),
-                                    viable_cell_count=get_property_from_sample(
-                                        sample, SampleProperty.VIABLE_CELLS
+                                    total_cell_density__cell_counter_=quantity_or_none(
+                                        TQuantityValueMillionCellsPerMilliliter,
+                                        row.total_cell_density,
                                     ),
-                                    average_total_cell_circularity=get_property_from_sample(
-                                        sample, SampleProperty.AVERAGE_CIRCULARITY
+                                    average_total_cell_diameter=quantity_or_none(
+                                        TQuantityValueMicrometer,
+                                        row.average_total_cell_diameter,
+                                    ),
+                                    viable_cell_count=quantity_or_none(
+                                        TQuantityValueCell, row.viable_cell_count
+                                    ),
+                                    average_total_cell_circularity=quantity_or_none(
+                                        TQuantityValueUnitless,
+                                        row.average_total_cell_circularity,
                                     ),
                                 ),
                             ]
--- tests/parsers/beckman_vi_cell_xr/to_allotrope_test.py
+++ tests/parsers/beckman_vi_cell_xr/to_allotrope_test.py
@@ -14,21 +14,20 @@ TESTDATA = f"{Path(__file__).parent}/testdata"
 
 class TestParser(ParserTest):
     VENDOR = VENDOR_TYPE
+    # OVERWRITE_ON_FAILURE = True
 
 
 def test_parse_vi_cell_xr_file_without_required_fields_then_raise() -> None:
     test_filepath = (
         f"{TESTDATA}/v2.04/Beckman_Vi-Cell-XR_example02_instrumentOutput_error.xls"
     )
-    expected_regex = re.escape(
-        "Expected to find lines with all of these headers: ['Viability (%)', 'Viable cells/ml (x10^6)']."
-    )
+    expected_regex = re.escape("Expected non-null value for Viability (%).")
     with pytest.raises(AllotropeConversionError, match=expected_regex):
         from_file(test_filepath, VENDOR_TYPE)
 
 
 def test_parse_vi_cell_xr_file_invalid_datetime_header_then_raise() -> None:
     test_filepath = f"{TESTDATA}/v2.06/Beckman_Vi-Cell-XR_example01_instrumentOutput_invalid_date_header_error.xlsx"
-    expected_regex = r"Unable to find key 'Sample date/time' in dataframe headers: .*"
+    expected_regex = r"Unable to find key 'Sample date' in dataframe headers: .*"
     with pytest.raises(AllotropeConversionError, match=expected_regex):
         from_file(test_filepath, VENDOR_TYPE)
