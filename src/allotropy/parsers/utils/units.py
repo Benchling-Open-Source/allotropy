a6326b02dd8448dbe48e5c570f30eba32be01e8d
Nathan Stender <nathan.stender@benchling.com>
2024-07-16 19:29:58 -0400

    refactor: Add Mapper to map from structured data to spectroscopy ASM model (#532)
    The idea here to to define a common structured data format for a given
    schema, and a mapper that translates that data to ASM
    
    That was, instead of every parser defining it's own structured data, it
    can instead map to the schema data class, and then get mapping for free.
    
    Apply this pattern to thermo nanodrop eight

--- src/allotropy/parsers/utils/units.py
+++ src/allotropy/parsers/utils/units.py
@@ -1,13 +1,23 @@
-from dataclasses import fields
+from dataclasses import fields, MISSING
 
 # We must import this to get all subclasses of TQuantityValue in scope
 import allotropy.allotrope.models.shared.definitions.custom  # noqa: F401
 from allotropy.allotrope.models.shared.definitions.definitions import TQuantityValue
 
+
+def _clean_unit(unit: str) -> str:
+    return unit.lower().replace("µ", "u")
+
+
+def _make_unit_entry(quantity_value: type[TQuantityValue]) -> str | None:
+    unit_field = next(field for field in fields(quantity_value) if field.name == "unit")
+    if unit_field.default == MISSING:
+        return None
+    return _clean_unit(str(unit_field.default))
+
+
 UNIT_TO_PROPERTY = {
-    next(
-        field for field in fields(quantity_value) if field.name == "unit"
-    ).default: quantity_value
+    _make_unit_entry(quantity_value): quantity_value
     for quantity_value in TQuantityValue.__subclasses__()
 }
 
@@ -15,4 +25,4 @@ UNIT_TO_PROPERTY = {
 def get_quantity_class(
     unit: str | None, default: type[TQuantityValue] | None = None
 ) -> type[TQuantityValue] | None:
-    return UNIT_TO_PROPERTY.get(unit, default)
+    return UNIT_TO_PROPERTY.get(_clean_unit(unit or ""), default)
