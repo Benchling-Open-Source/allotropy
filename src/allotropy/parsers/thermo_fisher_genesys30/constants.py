35be914476a9fe8a6fc6b592eaadb6613c595944
Pooja B <pooja.balaji@benchling.com>
2024-08-13 22:09:44 +0530

    Adding genesys 30 adapter scripts and test files

---
+++ src/allotropy/parsers/thermo_fisher_genesys30/constants.py
@@ -0,0 +1,13 @@
+""" Constants file for ThermoFisher Genesys 30 Adapter"""
+
+# Instrument software details
+GENESYS_SOFTWARE = "Genesys 30 instrument software"
+DISPLAY_NAME = "Thermo Fisher Genesys30"
+MODEL_NUMBER = "Genesys 30"
+BRAND_NAME = "Genesys"
+PRODUCT_MANUFACTURER = "Thermo Fisher Scientific"
+DEVICE_TYPE = "absorbance detector"
+
+# Error messages:
+UNSUPPORTED_FILE_FORMAT_ERROR = "Unsupported File Format. Expected .csv, .tsv. Actual: "
+MEASUREMENT_TIME_MISSING = "Unable to find measurement time from instrument file"
