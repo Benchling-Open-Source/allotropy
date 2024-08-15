a0165676d001551e7efd0495857de6405b0d9d20
Nathan Stender <nathan.stender@benchling.com>
2024-08-15 18:58:38 -0400

    feat: Support xlsx in unchained lunatic (#581)

--- src/allotropy/parsers/unchained_labs_lunatic/constants.py
+++ src/allotropy/parsers/unchained_labs_lunatic/constants.py
@@ -9,9 +9,8 @@ NO_WAVELENGTH_COLUMN_ERROR_MSG = (
 INCORRECT_WAVELENGTH_COLUMN_FORMAT_ERROR_MSG = (
     msg
 ) = "The wavelength_column should follow the pattern A### (ex. A260)."
-NO_DATE_OR_TIME_ERROR_MSG = (
-    "The file is required to include both 'Date' and 'Time' columns."
-)
+NO_DATE_OR_TIME_ERROR_MSG = "Unable to parse timestamp from input data, expected a 'Date' and 'Time' column in data or a 'Date' in metadata section."
+NO_DEVICE_IDENTIFIER_ERROR_MSG = "Unable to parse device identifier, expected an 'Instrument ID' column in data or 'Instrument' in metadata section."
 NO_MEASUREMENT_IN_PLATE_ERROR_MSG = (
     "The plate data does not contain absorbance measurement for {}."
 )
