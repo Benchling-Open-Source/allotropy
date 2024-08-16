053153edd79a5b08d381620273a66f2cd8cc4b57
Nathan Stender <nathan.stender@benchling.com>
2024-08-16 00:49:21 -0400

    hires done

--- src/allotropy/allotrope/schema_mappers/adm/cell_counting/benchling/_2023/_11/cell_counting.py
+++ src/allotropy/allotrope/schema_mappers/adm/cell_counting/benchling/_2023/_11/cell_counting.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable
 from dataclasses import dataclass
 
+from allotropy.allotrope.converter import add_custom_information_document
 from allotropy.allotrope.models.adm.cell_counting.benchling._2023._11.cell_counting import (
     CellCountingAggregateDocument,
     CellCountingDetectorDeviceControlAggregateDocument,
@@ -18,6 +19,7 @@ from allotropy.allotrope.models.adm.cell_counting.benchling._2023._11.cell_count
 )
 from allotropy.allotrope.models.shared.definitions.custom import (
     TQuantityValueCell,
+    TQuantityValueMicroliter,
     TQuantityValueMicrometer,
     TQuantityValueMillionCellsPerMilliliter,
     TQuantityValuePercent,
@@ -38,6 +40,9 @@ class Measurement:
     sample_identifier: str
     viability: float
     viable_cell_density: JsonFloat
+    batch_identifier: str | None = None
+    group_identifier: str | None = None
+    sample_draw_time: str | None = None
     dead_cell_density: JsonFloat | None = None
     cell_type_processing_method: str | None = None
     minimum_cell_diameter_setting: float | None = None
@@ -48,8 +53,17 @@ class Measurement:
     average_total_cell_diameter: JsonFloat | None = None
     average_live_cell_diameter: float | None = None
     viable_cell_count: float | None = None
+    dead_cell_count: float | None = None
     average_total_cell_circularity: float | None = None
     average_viable_cell_circularity: float | None = None
+    sample_volume_setting: float | None = None
+    average_compactness: float | None = None
+    average_area: float | None = None
+    average_perimeter: float | None = None
+    average_segment_area: float | None = None
+    total_object_count: float | None = None
+    standard_deviation: float | None = None
+    aggregate_rate: float | None = None
 
 
 @dataclass(frozen=True)
@@ -67,6 +81,11 @@ class Metadata:
     file_name: str
     equipment_serial_number: str | None = None
     software_version: str | None = None
+    product_manufacturer: str | None = None
+    brand_name: str | None = None
+    asset_management_identifier: str | None = None
+    device_identifier: str | None = None
+    description: str | None = None
 
 
 @dataclass
@@ -90,6 +109,11 @@ class Mapper:
             cell_counting_aggregate_document=CellCountingAggregateDocument(
                 device_system_document=DeviceSystemDocument(
                     model_number=data.metadata.model_number,
+                    product_manufacturer=data.metadata.product_manufacturer,
+                    brand_name=data.metadata.brand_name,
+                    asset_management_identifier=data.metadata.asset_management_identifier,
+                    device_identifier=data.metadata.device_identifier,
+                    description=data.metadata.description,
                     equipment_serial_number=data.metadata.equipment_serial_number,
                 ),
                 data_system_document=DataSystemDocument(
@@ -125,72 +149,122 @@ class Mapper:
         return CellCountingDetectorMeasurementDocumentItem(
             measurement_time=self.get_date_time(measurement.timestamp),
             measurement_identifier=measurement.measurement_identifier,
-            sample_document=SampleDocument(
-                sample_identifier=measurement.sample_identifier
-            ),
+            sample_document=self._get_sample_document(measurement),
             device_control_aggregate_document=CellCountingDetectorDeviceControlAggregateDocument(
                 device_control_document=[
                     DeviceControlDocumentItemModel(
                         device_type=metadata.device_type,
                         detection_type=metadata.detection_type,
+                        sample_volume_setting=quantity_or_none(
+                            TQuantityValueMicroliter, measurement.sample_volume_setting
+                        ),
                     )
                 ]
             ),
-            processed_data_aggregate_document=ProcessedDataAggregateDocument(
-                processed_data_document=[
-                    ProcessedDataDocumentItem(
-                        data_processing_document=DataProcessingDocument(
-                            cell_type_processing_method=measurement.cell_type_processing_method,
-                            minimum_cell_diameter_setting=quantity_or_none(
-                                TQuantityValueMicrometer,
-                                measurement.minimum_cell_diameter_setting,
-                            ),
-                            maximum_cell_diameter_setting=quantity_or_none(
-                                TQuantityValueMicrometer,
-                                measurement.maximum_cell_diameter_setting,
-                            ),
-                            cell_density_dilution_factor=quantity_or_none(
-                                TQuantityValueUnitless,
-                                measurement.cell_density_dilution_factor,
-                            ),
-                        ),
-                        viability__cell_counter_=TQuantityValuePercent(
-                            value=measurement.viability
-                        ),
-                        viable_cell_density__cell_counter_=TQuantityValueMillionCellsPerMilliliter(
-                            value=measurement.viable_cell_density
-                        ),
-                        dead_cell_density__cell_counter_=quantity_or_none(
-                            TQuantityValueMillionCellsPerMilliliter,
-                            measurement.dead_cell_density,
-                        ),
-                        total_cell_count=quantity_or_none(
-                            TQuantityValueCell, measurement.total_cell_count
-                        ),
-                        total_cell_density__cell_counter_=quantity_or_none(
-                            TQuantityValueMillionCellsPerMilliliter,
-                            measurement.total_cell_density,
-                        ),
-                        average_total_cell_diameter=quantity_or_none(
-                            TQuantityValueMicrometer,
-                            measurement.average_total_cell_diameter,
-                        ),
-                        average_live_cell_diameter__cell_counter_=quantity_or_none(
-                            TQuantityValueMicrometer,
-                            measurement.average_live_cell_diameter,
-                        ),
-                        viable_cell_count=quantity_or_none(
-                            TQuantityValueCell, measurement.viable_cell_count
-                        ),
-                        average_total_cell_circularity=quantity_or_none(
-                            TQuantityValueUnitless,
-                            measurement.average_total_cell_circularity,
-                        ),
-                        average_viable_cell_circularity=quantity_or_none(
-                            TQuantityValueUnitless,
-                            measurement.average_viable_cell_circularity,
-                        ),
-                    ),
-                ]
+            processed_data_aggregate_document=self._get_processed_data_aggregate_document(
+                measurement
             ),
+        )
+
+    def _get_sample_document(self, measurement: Measurement) -> SampleDocument:
+        custom_document = {
+            "group identifier": measurement.group_identifier,
+            "sample draw time": self.get_date_time(measurement.sample_draw_time)
+            if measurement.sample_draw_time
+            else None,
+        }
+        return add_custom_information_document(
+            SampleDocument(
+                sample_identifier=measurement.sample_identifier,
+                batch_identifier=measurement.batch_identifier,
+            ),
+            custom_document,
+        )
+
+    def _get_processed_data_aggregate_document(
+        self, measurement: Measurement
+    ) -> ProcessedDataAggregateDocument:
+        custom_document = {
+            "average compactness": quantity_or_none(
+                TQuantityValueUnitless, measurement.average_compactness
+            ),
+            "average area": quantity_or_none(
+                TQuantityValueUnitless, measurement.average_area
+            ),
+            "average perimeter": quantity_or_none(
+                TQuantityValueMicrometer, measurement.average_perimeter
+            ),
+            "average segment area": quantity_or_none(
+                TQuantityValueUnitless, measurement.average_segment_area
+            ),
+            "total object count": quantity_or_none(
+                TQuantityValueCell, measurement.total_object_count
+            ),
+            "standard deviation": quantity_or_none(
+                TQuantityValueCell, measurement.standard_deviation
+            ),
+            "aggregate rate": quantity_or_none(
+                TQuantityValuePercent, measurement.aggregate_rate
+            ),
+        }
+        processed_data_document = ProcessedDataDocumentItem(
+            data_processing_document=DataProcessingDocument(
+                cell_type_processing_method=measurement.cell_type_processing_method,
+                minimum_cell_diameter_setting=quantity_or_none(
+                    TQuantityValueMicrometer,
+                    measurement.minimum_cell_diameter_setting,
+                ),
+                maximum_cell_diameter_setting=quantity_or_none(
+                    TQuantityValueMicrometer,
+                    measurement.maximum_cell_diameter_setting,
+                ),
+                cell_density_dilution_factor=quantity_or_none(
+                    TQuantityValueUnitless,
+                    measurement.cell_density_dilution_factor,
+                ),
+            ),
+            viability__cell_counter_=TQuantityValuePercent(value=measurement.viability),
+            viable_cell_density__cell_counter_=TQuantityValueMillionCellsPerMilliliter(
+                value=measurement.viable_cell_density
+            ),
+            dead_cell_density__cell_counter_=quantity_or_none(
+                TQuantityValueMillionCellsPerMilliliter,
+                measurement.dead_cell_density,
+            ),
+            total_cell_count=quantity_or_none(
+                TQuantityValueCell, measurement.total_cell_count
+            ),
+            total_cell_density__cell_counter_=quantity_or_none(
+                TQuantityValueMillionCellsPerMilliliter,
+                measurement.total_cell_density,
+            ),
+            average_total_cell_diameter=quantity_or_none(
+                TQuantityValueMicrometer,
+                measurement.average_total_cell_diameter,
+            ),
+            average_live_cell_diameter__cell_counter_=quantity_or_none(
+                TQuantityValueMicrometer,
+                measurement.average_live_cell_diameter,
+            ),
+            viable_cell_count=quantity_or_none(
+                TQuantityValueCell, measurement.viable_cell_count
+            ),
+            dead_cell_count=quantity_or_none(
+                TQuantityValueCell, measurement.dead_cell_count
+            ),
+            average_total_cell_circularity=quantity_or_none(
+                TQuantityValueUnitless,
+                measurement.average_total_cell_circularity,
+            ),
+            average_viable_cell_circularity=quantity_or_none(
+                TQuantityValueUnitless,
+                measurement.average_viable_cell_circularity,
+            ),
+        )
+        return ProcessedDataAggregateDocument(
+            processed_data_document=[
+                add_custom_information_document(
+                    processed_data_document, custom_document
+                )
+            ]
         )
