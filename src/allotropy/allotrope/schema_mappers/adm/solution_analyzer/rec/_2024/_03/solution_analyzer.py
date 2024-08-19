ae3d89e3799ebb6c697eca1f367dc984eaaba4df
Nathan Stender <nathan.stender@benchling.com>
2024-08-19 17:41:56 -0400

    refactor: Use schema mapper for novabio flex (#587)

--- src/allotropy/allotrope/schema_mappers/adm/solution_analyzer/rec/_2024/_03/solution_analyzer.py
+++ src/allotropy/allotrope/schema_mappers/adm/solution_analyzer/rec/_2024/_03/solution_analyzer.py
@@ -1,9 +1,11 @@
 from collections.abc import Callable
 from dataclasses import dataclass
+from typing import Any
 
 from allotropy.allotrope.models.adm.solution_analyzer.rec._2024._03.solution_analyzer import (
     AnalyteAggregateDocument,
     AnalyteDocument,
+    DataProcessingDocument,
     DataSystemDocument,
     DeviceControlAggregateDocument,
     DeviceControlDocumentItem,
@@ -13,15 +15,26 @@ from allotropy.allotrope.models.adm.solution_analyzer.rec._2024._03.solution_ana
     MeasurementAggregateDocument,
     MeasurementDocument,
     Model,
+    ProcessedDataAggregateDocument,
+    ProcessedDataDocumentItem,
     SampleDocument,
     SolutionAnalyzerAggregateDocument,
     SolutionAnalyzerDocumentItem,
 )
 from allotropy.allotrope.models.shared.definitions.custom import (
+    TQuantityValueCell,
+    TQuantityValueDegreeCelsius,
     TQuantityValueGramPerLiter,
+    TQuantityValueMicrometer,
     TQuantityValueMilliAbsorbanceUnit,
     TQuantityValueMilliliterPerLiter,
+    TQuantityValueMillimeterOfMercury,
     TQuantityValueMillimolePerLiter,
+    TQuantityValueMillionCellsPerMilliliter,
+    TQuantityValueMilliOsmolesPerKilogram,
+    TQuantityValuePercent,
+    TQuantityValuePH,
+    TQuantityValueUnitless,
 )
 from allotropy.allotrope.models.shared.definitions.definitions import TDateTimeValue
 from allotropy.constants import ASM_CONVERTER_VERSION
@@ -34,6 +47,12 @@ class Analyte:
     name: str
     value: float
     unit: str
+
+    def __lt__(self, other: Any) -> bool:
+        if not isinstance(other, Analyte):
+            return False
+
+        return self.name < other.name
 
 
 @dataclass(frozen=True)
@@ -48,10 +67,27 @@ class Measurement:
     identifier: str
     measurement_time: str
     sample_identifier: str
+    description: str | None = None
     batch_identifier: str | None = None
+    detection_type: str | None = None
 
     # Measurements
     absorbance: float | None = None
+    po2: float | None = None
+    pco2: float | None = None
+    carbon_dioxide_saturation: float | None = None
+    oxygen_saturation: float | None = None
+    ph: float | None = None
+    temperature: float | None = None
+    osmolality: float | None = None
+    viability: float | None = None
+    total_cell_density: float | None = None
+    viable_cell_density: float | None = None
+    average_live_cell_diameter: float | None = None
+    total_cell_count: float | None = None
+    viable_cell_count: float | None = None
+    cell_type_processing_method: str | None = None
+    cell_density_dilution_factor: float | None = None
     analytes: list[Analyte] | None = None
 
     # Errors
@@ -110,10 +146,11 @@ class Mapper:
                     model_number=data.metadata.model_number,
                     equipment_serial_number=data.metadata.equipment_serial_number,
                     device_identifier=data.metadata.device_identifier,
+                    product_manufacturer=data.metadata.product_manufacturer,
                 ),
                 data_system_document=DataSystemDocument(
                     file_name=data.metadata.file_name,
-                    UNC_path="",
+                    UNC_path=data.metadata.unc_path,
                     software_name=data.metadata.software_name,
                     software_version=data.metadata.software_version,
                     ASM_converter_name=self.converter_name,
@@ -156,11 +193,9 @@ class Mapper:
                 device_control_document=[
                     DeviceControlDocumentItem(
                         device_type=metadata.device_type,
+                        detection_type=measurement.detection_type,
                     ),
                 ]
-            ),
-            absorbance=quantity_or_none(
-                TQuantityValueMilliAbsorbanceUnit, measurement.absorbance
             ),
             analyte_aggregate_document=AnalyteAggregateDocument(
                 analyte_document=[
@@ -170,8 +205,29 @@ class Mapper:
             )
             if measurement.analytes
             else None,
+            processed_data_aggregate_document=self._create_processed_data_document(
+                measurement
+            ),
             error_aggregate_document=self._get_error_aggregate_document(
                 measurement.errors
+            ),
+            absorbance=quantity_or_none(
+                TQuantityValueMilliAbsorbanceUnit, measurement.absorbance
+            ),
+            pO2=quantity_or_none(TQuantityValueMillimeterOfMercury, measurement.po2),
+            pCO2=quantity_or_none(TQuantityValueMillimeterOfMercury, measurement.pco2),
+            carbon_dioxide_saturation=quantity_or_none(
+                TQuantityValuePercent, measurement.carbon_dioxide_saturation
+            ),
+            oxygen_saturation=quantity_or_none(
+                TQuantityValuePercent, measurement.oxygen_saturation
+            ),
+            pH=quantity_or_none(TQuantityValuePH, measurement.ph),
+            temperature=quantity_or_none(
+                TQuantityValueDegreeCelsius, measurement.temperature
+            ),
+            osmolality=quantity_or_none(
+                TQuantityValueMilliOsmolesPerKilogram, measurement.osmolality
             ),
         )
 
@@ -179,6 +235,7 @@ class Mapper:
         return SampleDocument(
             sample_identifier=measurement.sample_identifier,
             batch_identifier=measurement.batch_identifier,
+            description=measurement.description,
         )
 
     def _create_analyte_document(self, analyte: Analyte) -> AnalyteDocument:
@@ -217,6 +274,46 @@ class Mapper:
 
         msg = f"Invalid unit for analyte: {analyte.unit}, value values are: g/L, mL/L, mmol/L"
         raise AllotropeConversionError(msg)
+
+    def _create_processed_data_document(
+        self, measurement: Measurement
+    ) -> ProcessedDataAggregateDocument | None:
+        processed_data_document = ProcessedDataDocumentItem(
+            viability__cell_counter_=quantity_or_none(
+                TQuantityValuePercent, measurement.viability
+            ),
+            total_cell_density__cell_counter_=quantity_or_none(
+                TQuantityValueMillionCellsPerMilliliter, measurement.total_cell_density
+            ),
+            viable_cell_density__cell_counter_=quantity_or_none(
+                TQuantityValueMillionCellsPerMilliliter, measurement.viable_cell_density
+            ),
+            average_live_cell_diameter__cell_counter_=quantity_or_none(
+                TQuantityValueMicrometer, measurement.average_live_cell_diameter
+            ),
+            total_cell_count=quantity_or_none(
+                TQuantityValueCell, measurement.total_cell_count
+            ),
+            viable_cell_count=quantity_or_none(
+                TQuantityValueCell, measurement.viable_cell_count
+            ),
+            data_processing_document=DataProcessingDocument(
+                cell_type_processing_method=measurement.cell_type_processing_method,
+                cell_density_dilution_factor=quantity_or_none(
+                    TQuantityValueUnitless, measurement.cell_density_dilution_factor
+                ),
+            )
+            if measurement.cell_type_processing_method
+            or measurement.cell_density_dilution_factor is not None
+            else None,
+        )
+
+        if all(value is None for value in processed_data_document.__dict__.values()):
+            return None
+
+        return ProcessedDataAggregateDocument(
+            processed_data_document=[processed_data_document]
+        )
 
     def _get_error_aggregate_document(
         self, errors: list[Error] | None
