f70b2f93b2b71ea620c69743cd5d7b1ac993602f
Alejandro Salgado <alejando.salgado@benchling.com>
2024-08-22 15:27:20 -0500

    add device and data system documents to nanodrop one

--- src/allotropy/parsers/thermo_fisher_nanodrop_one/thermo_fisher_nanodrop_one_parser.py
+++ src/allotropy/parsers/thermo_fisher_nanodrop_one/thermo_fisher_nanodrop_one_parser.py
@@ -1,13 +1,14 @@
-from typing import Any
-
 from allotropy.allotrope.models.adm.spectrophotometry.benchling._2023._12.spectrophotometry import (
     Model,
+)
+from allotropy.allotrope.schema_mappers.adm.spectrophotometry.benchling._2023._12.spectrophotometry import (
+    Mapper,
 )
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.release_state import ReleaseState
 from allotropy.parsers.thermo_fisher_nanodrop_one.constants import DISPLAY_NAME
 from allotropy.parsers.thermo_fisher_nanodrop_one.thermo_fisher_nanodrop_one_structure import (
-    Data,
+    create_data,
 )
 from allotropy.parsers.utils.pandas import read_multisheet_excel
 from allotropy.parsers.vendor_parser import VendorParser
@@ -22,15 +23,10 @@ class ThermoFisherNanodropOneParser(VendorParser):
     def release_state(self) -> ReleaseState:
         return ReleaseState.WORKING_DRAFT
 
-    def to_allotrope(self, named_file_contents: NamedFileContents) -> Any:
+    def to_allotrope(self, named_file_contents: NamedFileContents) -> Model:
         contents = read_multisheet_excel(
             named_file_contents.contents,
             engine="calamine",
         )
-        data = Data.create(contents)
-        return self._get_model(data)
-
-    def _get_model(self, _: Data) -> Model:
-        return Model(
-            field_asm_manifest="http://purl.allotrope.org/manifests/spectrophotometry/BENCHLING/2023/12/spectrophotometry.manifest",
-        )
+        data = create_data(contents, named_file_contents.original_file_name)
+        return self._get_mapper(Mapper).map_model(data)
--- src/allotropy/parsers/thermo_fisher_nanodrop_one/thermo_fisher_nanodrop_one_structure.py
+++ src/allotropy/parsers/thermo_fisher_nanodrop_one/thermo_fisher_nanodrop_one_structure.py
@@ -1,12 +1,21 @@
-from __future__ import annotations
-
-from dataclasses import dataclass
-
 import pandas as pd
 
+from allotropy.allotrope.schema_mappers.adm.spectrophotometry.benchling._2023._12.spectrophotometry import (
+    Data,
+    Metadata,
+)
 
-@dataclass(frozen=True)
-class Data:
-    @staticmethod
-    def create(_: dict[str, pd.DataFrame]) -> Data:
-        return Data()
+
+def create_data(_: dict[str, pd.DataFrame], file_name: str) -> Data:
+    return Data(
+        metadata=Metadata(
+            device_identifier="N/A",
+            device_type="",
+            model_number="NanoDrop One",
+            brand_name="NanoDrop",
+            product_manufacturer="ThermoFisher Scientific",
+            file_name=file_name,
+            software_name="NanoDrop One software",
+        ),
+        measurement_groups=[],
+    )
