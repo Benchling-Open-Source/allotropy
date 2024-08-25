9ca89a89be876498e11f3fe5778e68a36e4bb381
Nathan Stender <nathan.stender@benchling.com>
2024-08-25 12:00:23 -0400

    Undo change:

--- tests/to_allotrope_test.py
+++ tests/to_allotrope_test.py
@@ -9,19 +9,16 @@ from allotropy.testing.utils import from_file, validate_contents
 from allotropy.to_allotrope import allotrope_from_file, allotrope_model_from_file
 
 INVALID_FILE_PATH = "not/a/path"
+EXPECTED_ERROR_MESSAGE = f"File not found: {INVALID_FILE_PATH}"
 
 
 def test_allotrope_from_file_not_found() -> None:
-    with pytest.raises(
-        AllotropeConversionError, match=f"File not found: {INVALID_FILE_PATH}"
-    ):
+    with pytest.raises(AllotropeConversionError, match=EXPECTED_ERROR_MESSAGE):
         allotrope_from_file(INVALID_FILE_PATH, Vendor.AGILENT_GEN5)
 
 
 def test_allotrope_model_from_file_not_found() -> None:
-    with pytest.raises(
-        AllotropeConversionError, match=f"File not found: {INVALID_FILE_PATH}"
-    ):
+    with pytest.raises(AllotropeConversionError, match=EXPECTED_ERROR_MESSAGE):
         allotrope_model_from_file(INVALID_FILE_PATH, Vendor.AGILENT_GEN5)
 
 
