0bc1c7f8ac58e9952e4941b7bb4e939b92f7ef1c
alejandro-salgado <alejando.salgado@benchling.com>
2024-09-20 16:09:56 -0500

    feat: Improve appbio quantstudio design and analysis (#659)
    - Simplify sheets needed to infer presence/absence experiment type
    inference in Appbio Quantstudio Design and Analysis
    - Allow software name and version to be None in Appbio Quantstudio
    Design and Analysis

--- src/allotropy/parsers/appbio_quantstudio_designandanalysis/structure/generic/structure.py
+++ src/allotropy/parsers/appbio_quantstudio_designandanalysis/structure/generic/structure.py
@@ -66,11 +66,15 @@ class Header:
 
     @staticmethod
     def create(header: SeriesData) -> Header:
-        software_info = assert_not_none(
-            re.match(
-                "(.*) v(.+)",
-                header[str, "Software Name and Version"],
-            )
+        software_info = re.match(
+            "(.*) v(.+)",
+            header.get(str, "Software Name and Version", ""),
+        )
+
+        software_name, software_version = (
+            (software_info.group(1), software_info.group(2))
+            if software_info
+            else (None, None)
         )
 
         stage_number_raw = header.get(str, "PCR Stage/Step Number", "")
@@ -110,8 +114,8 @@ class Header:
             block_serial_number=header.get(str, "Block Serial Number"),
             heated_cover_serial_number=header.get(str, "Heated Cover Serial Number"),
             pcr_stage_number=pcr_stage_number,
-            software_name=software_info.group(1),
-            software_version=software_info.group(2),
+            software_name=software_name,
+            software_version=software_version,
         )
 
 
