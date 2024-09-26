73926dc3221fea1831c1e6d6909c36f8737a46cb
Nathan Stender <nathan.stender@benchling.com>
2024-09-26 12:12:10 -0400

    Remove CSV

--- src/allotropy/parsers/nexcelom_matrix/nexcelom_matrix_reader.py
+++ src/allotropy/parsers/nexcelom_matrix/nexcelom_matrix_reader.py
@@ -1,30 +1,21 @@
 import pandas as pd
 
-from allotropy.constants import DEFAULT_ENCODING
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.nexcelom_matrix.constants import (
     MILLION_CONVERSION,
     MILLION_SCALE_COLS,
 )
 from allotropy.parsers.utils.pandas import (
-    read_csv,
     read_excel,
 )
 
 
 class NexcelomMatrixReader:
-    SUPPORTED_EXTENSIONS = "csv,xlsx"
+    SUPPORTED_EXTENSIONS = "xlsx"
     data: pd.DataFrame
 
     def __init__(self, named_file_contents: NamedFileContents) -> None:
-        if named_file_contents.extension == "csv":
-            df = read_csv(
-                named_file_contents.contents,
-                index_col=False,
-                encoding=named_file_contents.encoding,
-            )
-        else:
-            df = read_excel(named_file_contents.contents, encoding=named_file_contents.encoding)
+        df = read_excel(named_file_contents.contents)
 
         df[MILLION_SCALE_COLS] = df[MILLION_SCALE_COLS].map(
             lambda x: x / MILLION_CONVERSION
