c63de73c3dc77830c439fd31c085384ab17cba4c
Joshua Hernandez <jherna@benchling.com>
2024-09-26 11:33:32 -0500

    changed thw way to handle optional and reuqired fields

--- src/allotropy/parsers/chemometec_nc_view/chemometec_nc_view_structure.py
+++ src/allotropy/parsers/chemometec_nc_view/chemometec_nc_view_structure.py
@@ -1,9 +1,5 @@
 from decimal import Decimal
 
-from allotropy.allotrope.models.shared.definitions.definitions import (
-    JsonFloat,
-    NaN,
-)
 from allotropy.allotrope.schema_mappers.adm.cell_counting.benchling._2023._11.cell_counting import (
     Measurement,
     MeasurementGroup,
@@ -13,7 +9,7 @@ from allotropy.parsers.chemometec_nc_view import constants
 from allotropy.parsers.constants import NOT_APPLICABLE
 from allotropy.parsers.utils.pandas import SeriesData
 from allotropy.parsers.utils.uuids import random_uuid_str
-from allotropy.parsers.utils.values import try_float_or_nan, try_float_or_none
+from allotropy.parsers.utils.values import try_float_or_none
 
 
 def create_metadata(data: SeriesData, file_name: str) -> Metadata:
@@ -30,6 +26,21 @@ def create_metadata(data: SeriesData, file_name: str) -> Metadata:
 
 
 def create_measurement_groups(data: SeriesData) -> MeasurementGroup:
+    # Cell counts are measured in cells/mL, but reported in millions of cells/mL
+    viable_cell_density = float(
+        Decimal(_format_unit(data[str, "LIVE (cells/ml)"])) / Decimal("1000000")
+    )
+    total_cell_density_val = data.get(str, "TOTAL (cells/ml)")
+    if total_cell_density_val:
+        total_cell_density = float(
+            Decimal(_format_unit(total_cell_density_val)) / Decimal("1000000")
+        )
+    dead_cell_density_val = data.get(str, "DEAD (cells/ml)")
+    if dead_cell_density_val:
+        dead_cell_density = float(
+            Decimal(_format_unit(dead_cell_density_val)) / Decimal("1000000")
+        )
+
     return MeasurementGroup(
         analyst=data[str, "OPERATOR"],
         measurements=[
@@ -39,11 +50,9 @@ def create_measurement_groups(data: SeriesData) -> MeasurementGroup:
                 timestamp=_format_timestamp(data[str, "NAME"]),
                 sample_identifier=data[str, "SAMPLE ID"],
                 viability=data[float, "VIABILITY (%)"],
-                total_cell_density=_format_number(data.get(str, "TOTAL (cells/ml)")),
-                viable_cell_density=_format_required_number(
-                    data.get(str, "LIVE (cells/ml)")
-                ),
-                dead_cell_density=_format_number(data.get(str, "DEAD (cells/ml)")),
+                total_cell_density=total_cell_density,
+                viable_cell_density=viable_cell_density,
+                dead_cell_density=dead_cell_density,
                 average_total_cell_diameter=try_float_or_none(
                     data.get(str, "DIAMETER (μm)")
                 ),
@@ -57,20 +66,8 @@ def create_measurement_groups(data: SeriesData) -> MeasurementGroup:
     )
 
 
-def _format_number(unit: str | None) -> JsonFloat | None:
-    if not unit:
-        return None
-    number = try_float_or_none("".join(unit.split()))
-    return float(Decimal(number) / Decimal("1000000")) if number else None
-
-
-def _format_required_number(unit: str | None) -> JsonFloat:
-    if not unit:
-        return NaN
-    number = try_float_or_nan("".join(unit.split()))
-    if number is NaN:
-        return NaN
-    return float(Decimal(str(number)) / Decimal("1000000"))
+def _format_unit(unit: str) -> float:
+    return float("".join(unit.split()))
 
 
 def _format_timestamp(timestamp: str) -> str:
