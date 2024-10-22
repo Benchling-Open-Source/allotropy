35ffed769531f55470529a1886b2794372408a46
Nathan Stender <nathan.stender@benchling.com>
2024-10-22 09:20:25 -0400

    Remove comment from tutorial

--- src/allotropy/parsers/cfxmaestro/cfxmaestro_reader.py
+++ src/allotropy/parsers/cfxmaestro/cfxmaestro_reader.py
@@ -11,7 +11,4 @@ class CFXMaestroReader:
     data: pd.DataFrame
 
     def __init__(self, named_file_contents: NamedFileContents) -> None:
-        # Example of one way to parse data. This may work if your data is a simple dataset,
-        # but will probably need some modification, or a totally different approach if the input
-        # file is a different format.
         self.data = read_csv(named_file_contents.contents)
