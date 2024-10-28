40ed75da4ff0de2a009c7cbe429fe229b62ddc89
keerthna-ramachandran <keerthna.ramachandran@benchling.com>
2024-10-28 22:22:04 +0530

    feat: Binding Affinity Analyzer - initial version of schema (#740)
    Co-authored-by: Nathan Stender <nathan.stender@benchling.com>

--- src/allotropy/parsers/utils/units.py
+++ src/allotropy/parsers/utils/units.py
@@ -6,7 +6,7 @@ from allotropy.allotrope.models.shared.definitions.definitions import TQuantityV
 
 
 def _clean_unit(unit: str) -> str:
-    return unit.lower().replace("µ", "u").replace("μ", "u")
+    return unit.replace("µ", "u").replace("μ", "u")
 
 
 def _make_unit_entry(quantity_value: type[TQuantityValue]) -> str | None:
@@ -17,12 +17,30 @@ def _make_unit_entry(quantity_value: type[TQuantityValue]) -> str | None:
 
 
 UNIT_TO_PROPERTY = {
-    _make_unit_entry(quantity_value): quantity_value
-    for quantity_value in TQuantityValue.__subclasses__()
+    unit_entry: quantity_value
+    for unit_entry, quantity_value in [
+        (_make_unit_entry(quantity_value), quantity_value)
+        for quantity_value in TQuantityValue.__subclasses__()
+    ]
+    if unit_entry
+}
+
+# Some input files have units with incorrect capitalization. Try to handle this by checking with case insensitive.
+# We don't do this by default, however, because there are some cases where capitalization matters, e.g.
+# namometer (nm) != nanomolar (nM).
+LOWER_UNIT_TO_PROPERTY = {
+    unit.lower(): quantity_value for unit, quantity_value in UNIT_TO_PROPERTY.items()
 }
 
 
 def get_quantity_class(
     unit: str | None, default: type[TQuantityValue] | None = None
 ) -> type[TQuantityValue] | None:
-    return UNIT_TO_PROPERTY.get(_clean_unit(unit or ""), default)
+    clean_unit = _clean_unit(unit or "")
+    # Try to get case-sensitive unit, falling back to default if provided, finally checking case-insensitive unit,
+    # which may not always have the correct result.
+    return (
+        UNIT_TO_PROPERTY.get(clean_unit)
+        or default
+        or LOWER_UNIT_TO_PROPERTY.get(clean_unit.lower())
+    )
