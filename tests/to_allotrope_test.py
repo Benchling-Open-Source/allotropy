4bb45acafe12abefe42713dad2e91375048c9832
Nathan Stender <nathan.stender@benchling.com>
2024-11-08 12:15:55 -0500

    feat: Internal - add function to SeriesData to get unread keys (#766)
    This PR:
    
    1. Adds a function to `SeriesData` that returns all keys not read from
    from the pandas series when the function is called
    2. Adds a config flag `--warn_unused_keys` that prints warnings in tests
    if and keys are unread

--- tests/to_allotrope_test.py
+++ tests/to_allotrope_test.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import re
 
@@ -42,7 +43,11 @@ class ParserTest:
     VENDOR: Vendor
 
     # test_file_path is automatically populated with all files in testdata folder next to the test file.
-    def test_positive_cases(self, test_file_path: Path, *, overwrite: bool) -> None:
+    def test_positive_cases(
+        self, test_file_path: Path, *, overwrite: bool, warn_unread_keys: bool
+    ) -> None:
+        if warn_unread_keys:
+            os.environ["WARN_UNUSED_KEYS"] = "1"
         expected_filepath = test_file_path.with_suffix(".json")
         allotrope_dict = from_file(
             str(test_file_path), self.VENDOR, encoding=CHARDET_ENCODING
