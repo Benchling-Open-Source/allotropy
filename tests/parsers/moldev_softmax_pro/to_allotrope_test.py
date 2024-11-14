12bac089a89c0ccc8ab906660485cba356caf11b
Sebastian Lopez <sebastian.cardona@benchling.com>
2024-11-14 16:29:03 -0500

    add check for expected rows in plate data table

--- tests/parsers/moldev_softmax_pro/to_allotrope_test.py
+++ tests/parsers/moldev_softmax_pro/to_allotrope_test.py
@@ -47,6 +47,14 @@ def test_missing_kinetic_measurement() -> None:
         from_file(f"{TESTDATA}/errors/missing_kinetic_measurement.txt", VENDOR_TYPE)
 
 
+def test_invalid_measurement_table_dimensions() -> None:
+    with pytest.raises(
+        AllotropeConversionError,
+        match="Expected 4 rows in measurement table, got 3.",
+    ):
+        from_file(f"{TESTDATA}/errors/missing_measurement_table_row.txt", VENDOR_TYPE)
+
+
 @pytest.mark.parametrize(
     "test_file",
     [
