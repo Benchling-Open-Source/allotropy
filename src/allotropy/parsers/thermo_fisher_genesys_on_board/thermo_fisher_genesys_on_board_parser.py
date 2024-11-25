d8f31c1cad997f7fe923759405d431992a7f1b44
Sebastian Lopez <sebastian.cardona@benchling.com>
2024-11-25 12:37:46 -0500

    add software name

--- src/allotropy/parsers/thermo_fisher_genesys_on_board/thermo_fisher_genesys_on_board_parser.py
+++ src/allotropy/parsers/thermo_fisher_genesys_on_board/thermo_fisher_genesys_on_board_parser.py
@@ -7,7 +7,10 @@ from allotropy.allotrope.schema_mappers.adm.spectrophotometry.benchling._2023._1
 )
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.release_state import ReleaseState
-from allotropy.parsers.thermo_fisher_genesys_on_board.constants import DISPLAY_NAME
+from allotropy.parsers.thermo_fisher_genesys_on_board.constants import (
+    DISPLAY_NAME,
+    SOFTWARE_NAME,
+)
 from allotropy.parsers.thermo_fisher_visionlite.thermo_fisher_visionlite_reader import (
     ThermoFisherVisionliteReader,
 )
@@ -28,4 +31,5 @@ class ThermoFisherGenesysOnBoardParser(VendorParser[Data, Model]):
         return VisionLiteData.create(
             ThermoFisherVisionliteReader(named_file_contents),
             named_file_contents.original_file_path,
+            software_name=SOFTWARE_NAME,
         )
