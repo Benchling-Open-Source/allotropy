86d2f2caa1c50b5078ee033cd9f1915d8b422a63
Alejandro Salgado <alejando.salgado@benchling.com>
2024-11-26 11:39:32 -0500

    add flow measurement to unicorn

--- src/allotropy/parsers/cytiva_unicorn/structure/measurement_group.py
+++ src/allotropy/parsers/cytiva_unicorn/structure/measurement_group.py
@@ -23,6 +23,9 @@ from allotropy.parsers.cytiva_unicorn.structure.measurements.concentration impor
 from allotropy.parsers.cytiva_unicorn.structure.measurements.conductivity import (
     ConductivityMeasurement,
 )
+from allotropy.parsers.cytiva_unicorn.structure.measurements.flow import (
+    FlowMeasurement,
+)
 from allotropy.parsers.cytiva_unicorn.structure.measurements.generic import (
     UnicornMeasurement,
 )
@@ -47,49 +50,12 @@ def create_measurement_groups(
 
     static_docs = StaticDocs.create(handler, curves.find("Curve"), results)
 
-    sample_flow_cv_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="sample flow",
-        unit="CV/h",
-    )
-
-    system_flow_cv_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="system flow",
-        unit="CV/h",
-    )
-
-    sample_flow_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="sample flow",
-        unit="mL/min",
-    )
-
-    system_flow_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="system flow",
-        unit="mL/min",
-    )
-
-    sample_linear_flow_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="sample flow",
-        unit="cm/s",
-    )
-
     temperature_profile_component = DataCubeComponent(
         type_=FieldComponentDatatype.float,
         concept="temperature",
         unit="degC",
     )
 
-    sample_flow_cv_data_cube = handler.filter_curve(elements, r"^Sample flow \(CV/h\)$")
-    system_flow_cv_data_cube = handler.filter_curve(elements, r"^System flow \(CV/h\)$")
-    sample_flow_data_cube = handler.filter_curve(elements, r"^Sample flow$")
-    system_flow_data_cube = handler.filter_curve(elements, r"^System flow$")
-    sample_linear_flow_data_cube = handler.filter_curve(
-        elements, r"^Sample linear flow$"
-    )
     temperature_profile_data_cube = handler.filter_curve(elements, r"^Cond temp$")
 
     return [
@@ -102,44 +68,7 @@ def create_measurement_groups(
                 PhMeasurement.create(handler, elements, static_docs),
                 ConcentrationMeasurement.create(handler, elements, static_docs),
                 PressureMeasurement.create(handler, elements, static_docs),
-                Measurement(
-                    measurement_identifier=random_uuid_str(),
-                    chromatography_column_doc=static_docs.chromatography_doc,
-                    injection_doc=static_docs.injection_doc,
-                    sample_doc=static_docs.sample_doc,
-                    device_control_docs=[
-                        DeviceControlDoc(
-                            device_type=DEVICE_TYPE,
-                            sample_flow_data_cube=UnicornMeasurement.create_data_cube(
-                                handler,
-                                sample_flow_cv_data_cube,
-                                sample_flow_cv_component,
-                            ),
-                            system_flow_data_cube=UnicornMeasurement.create_data_cube(
-                                handler,
-                                system_flow_cv_data_cube,
-                                system_flow_cv_component,
-                            ),
-                        ),
-                        DeviceControlDoc(
-                            device_type=DEVICE_TYPE,
-                            sample_flow_data_cube=UnicornMeasurement.create_data_cube(
-                                handler, sample_flow_data_cube, sample_flow_component
-                            ),
-                            system_flow_data_cube=UnicornMeasurement.create_data_cube(
-                                handler, system_flow_data_cube, system_flow_component
-                            ),
-                        ),
-                        DeviceControlDoc(
-                            device_type=DEVICE_TYPE,
-                            sample_flow_data_cube=UnicornMeasurement.create_data_cube(
-                                handler,
-                                sample_linear_flow_data_cube,
-                                sample_linear_flow_component,
-                            ),
-                        ),
-                    ],
-                ),
+                FlowMeasurement.create(handler, elements, static_docs),
                 Measurement(
                     measurement_identifier=random_uuid_str(),
                     chromatography_column_doc=static_docs.chromatography_doc,
---
+++ src/allotropy/parsers/cytiva_unicorn/structure/measurements/flow.py
@@ -0,0 +1,92 @@
+from allotropy.allotrope.models.shared.definitions.definitions import (
+    FieldComponentDatatype,
+)
+from allotropy.allotrope.schema_mappers.adm.liquid_chromatography.benchling._2023._09.liquid_chromatography import (
+    DataCubeComponent,
+    DeviceControlDoc,
+    Measurement,
+)
+from allotropy.parsers.cytiva_unicorn.constants import DEVICE_TYPE
+from allotropy.parsers.cytiva_unicorn.cytiva_unicorn_reader import (
+    StrictElement,
+    UnicornFileHandler,
+)
+from allotropy.parsers.cytiva_unicorn.structure.measurements.generic import (
+    UnicornMeasurement,
+)
+from allotropy.parsers.cytiva_unicorn.structure.static_docs import (
+    StaticDocs,
+)
+from allotropy.parsers.utils.uuids import random_uuid_str
+
+
+class FlowMeasurement(UnicornMeasurement):
+    @classmethod
+    def create(
+        cls,
+        handler: UnicornFileHandler,
+        elements: list[StrictElement],
+        static_docs: StaticDocs,
+    ) -> Measurement:
+        return Measurement(
+            measurement_identifier=random_uuid_str(),
+            chromatography_column_doc=static_docs.chromatography_doc,
+            injection_doc=static_docs.injection_doc,
+            sample_doc=static_docs.sample_doc,
+            device_control_docs=[
+                DeviceControlDoc(
+                    device_type=DEVICE_TYPE,
+                    sample_flow_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^Sample flow \(CV/h\)$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="sample flow",
+                            unit="CV/h",
+                        ),
+                    ),
+                    system_flow_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^System flow \(CV/h\)$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="system flow",
+                            unit="CV/h",
+                        ),
+                    ),
+                ),
+                DeviceControlDoc(
+                    device_type=DEVICE_TYPE,
+                    sample_flow_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^Sample flow$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="sample flow",
+                            unit="mL/min",
+                        ),
+                    ),
+                    system_flow_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^System flow$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="system flow",
+                            unit="mL/min",
+                        ),
+                    ),
+                ),
+                DeviceControlDoc(
+                    device_type=DEVICE_TYPE,
+                    sample_flow_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^Sample linear flow$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="sample flow",
+                            unit="cm/s",
+                        ),
+                    ),
+                ),
+            ],
+        )
