167d1ea67d9ed862f6fceb8ff3723d3e5f75fe92
Alejandro Salgado <alejando.salgado@benchling.com>
2024-11-26 11:44:12 -0500

    add temperature measurement to unicorn

--- src/allotropy/parsers/cytiva_unicorn/structure/measurement_group.py
+++ src/allotropy/parsers/cytiva_unicorn/structure/measurement_group.py
@@ -1,13 +1,6 @@
-from allotropy.allotrope.models.shared.definitions.definitions import (
-    FieldComponentDatatype,
-)
 from allotropy.allotrope.schema_mappers.adm.liquid_chromatography.benchling._2023._09.liquid_chromatography import (
-    DataCubeComponent,
-    DeviceControlDoc,
-    Measurement,
     MeasurementGroup,
 )
-from allotropy.parsers.cytiva_unicorn.constants import DEVICE_TYPE
 from allotropy.parsers.cytiva_unicorn.cytiva_unicorn_reader import (
     StrictElement,
     UnicornFileHandler,
@@ -26,19 +19,18 @@ from allotropy.parsers.cytiva_unicorn.structure.measurements.conductivity import
 from allotropy.parsers.cytiva_unicorn.structure.measurements.flow import (
     FlowMeasurement,
 )
-from allotropy.parsers.cytiva_unicorn.structure.measurements.generic import (
-    UnicornMeasurement,
-)
 from allotropy.parsers.cytiva_unicorn.structure.measurements.ph import (
     PhMeasurement,
 )
 from allotropy.parsers.cytiva_unicorn.structure.measurements.pressure import (
     PressureMeasurement,
 )
+from allotropy.parsers.cytiva_unicorn.structure.measurements.temperature import (
+    TemperatureMeasurement,
+)
 from allotropy.parsers.cytiva_unicorn.structure.static_docs import (
     StaticDocs,
 )
-from allotropy.parsers.utils.uuids import random_uuid_str
 
 
 def create_measurement_groups(
@@ -49,14 +41,6 @@ def create_measurement_groups(
     elements = curves.findall("Curve")
 
     static_docs = StaticDocs.create(handler, curves.find("Curve"), results)
-
-    temperature_profile_component = DataCubeComponent(
-        type_=FieldComponentDatatype.float,
-        concept="temperature",
-        unit="degC",
-    )
-
-    temperature_profile_data_cube = handler.filter_curve(elements, r"^Cond temp$")
 
     return [
         MeasurementGroup(
@@ -69,22 +53,7 @@ def create_measurement_groups(
                 ConcentrationMeasurement.create(handler, elements, static_docs),
                 PressureMeasurement.create(handler, elements, static_docs),
                 FlowMeasurement.create(handler, elements, static_docs),
-                Measurement(
-                    measurement_identifier=random_uuid_str(),
-                    chromatography_column_doc=static_docs.chromatography_doc,
-                    injection_doc=static_docs.injection_doc,
-                    sample_doc=static_docs.sample_doc,
-                    device_control_docs=[
-                        DeviceControlDoc(
-                            device_type=DEVICE_TYPE,
-                            temperature_profile_data_cube=UnicornMeasurement.create_data_cube(
-                                handler,
-                                temperature_profile_data_cube,
-                                temperature_profile_component,
-                            ),
-                        ),
-                    ],
-                ),
+                TemperatureMeasurement.create(handler, elements, static_docs),
             ]
         )
     ]
---
+++ src/allotropy/parsers/cytiva_unicorn/structure/measurements/temperature.py
@@ -0,0 +1,50 @@
+from allotropy.allotrope.models.shared.definitions.definitions import (
+    FieldComponentDatatype,
+)
+from allotropy.allotrope.schema_mappers.adm.liquid_chromatography.benchling._2023._09.liquid_chromatography import (
+    DataCubeComponent,
+    DeviceControlDoc,
+    Measurement,
+)
+from allotropy.parsers.cytiva_unicorn.constants import DEVICE_TYPE
+from allotropy.parsers.cytiva_unicorn.cytiva_unicorn_reader import (
+    StrictElement,
+    UnicornFileHandler,
+)
+from allotropy.parsers.cytiva_unicorn.structure.measurements.generic import (
+    UnicornMeasurement,
+)
+from allotropy.parsers.cytiva_unicorn.structure.static_docs import (
+    StaticDocs,
+)
+from allotropy.parsers.utils.uuids import random_uuid_str
+
+
+class TemperatureMeasurement(UnicornMeasurement):
+    @classmethod
+    def create(
+        cls,
+        handler: UnicornFileHandler,
+        elements: list[StrictElement],
+        static_docs: StaticDocs,
+    ) -> Measurement:
+        return Measurement(
+            measurement_identifier=random_uuid_str(),
+            chromatography_column_doc=static_docs.chromatography_doc,
+            injection_doc=static_docs.injection_doc,
+            sample_doc=static_docs.sample_doc,
+            device_control_docs=[
+                DeviceControlDoc(
+                    device_type=DEVICE_TYPE,
+                    temperature_profile_data_cube=UnicornMeasurement.create_data_cube(
+                        handler,
+                        handler.filter_curve(elements, r"^Cond temp$"),
+                        DataCubeComponent(
+                            type_=FieldComponentDatatype.float,
+                            concept="temperature",
+                            unit="degC",
+                        ),
+                    ),
+                ),
+            ],
+        )
