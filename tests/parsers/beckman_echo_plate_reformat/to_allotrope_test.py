ef27504f5280cd130bbe488b9ceba868eb35ceeb
Erik Snow <esnow@cellinobio.com>
2024-12-12 18:07:51 -0500

    update test case files and add vscode config files to .gitignore

--- tests/parsers/beckman_echo_plate_reformat/to_allotrope_test.py
+++ tests/parsers/beckman_echo_plate_reformat/to_allotrope_test.py
@@ -1,13 +1,9 @@
-import pytest
-
-from allotropy.exceptions import AllotropeConversionError
 from allotropy.parser_factory import Vendor
-from allotropy.testing.utils import from_file, get_testdata_dir, validate_contents
+from allotropy.testing.utils import get_testdata_dir
 from tests.to_allotrope_test import ParserTest
 
 VENDOR_TYPE = Vendor.BECKMAN_ECHO_PLATE_REFORMAT
 TESTDATA = get_testdata_dir(__file__)
-
 
 class TestParser(ParserTest):
     VENDOR = VENDOR_TYPE
