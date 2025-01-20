695c78951d73d45647a78bd42371cfbf7738a9e9
aishwarya <aishwarya.a@benchling.com>
2025-01-20 12:59:32 +0530

    resolving merge conflicts

--- src/allotropy/allotrope/schema_mappers/adm/liquid_chromatography/benchling/_2023/_09/liquid_chromatography.py
+++ src/allotropy/allotrope/schema_mappers/adm/liquid_chromatography/benchling/_2023/_09/liquid_chromatography.py
@@ -40,7 +40,6 @@ from allotropy.allotrope.models.shared.definitions.custom import (
     TQuantityValuePercent,
     TQuantityValueSecondTime,
     TQuantityValueUnitless,
-    TQuantityValueUnitless,
 )
 from allotropy.allotrope.models.shared.definitions.definitions import TDatacube
 from allotropy.allotrope.schema_mappers.data_cube import (
@@ -342,50 +341,51 @@ class Mapper(SchemaMapper[Data, Model]):
     def _get_peak_document(self, peak: Peak) -> PeakDocument:
         return add_custom_information_document(
             PeakDocument(
-            identifier=peak.identifier,
-            peak_index=peak.index,
-            peak_start=quantity_or_none_from_unit(peak.start_unit, peak.start),  # type: ignore[arg-type]
-            peak_end=quantity_or_none_from_unit(peak.end_unit, peak.end),  # type: ignore[arg-type]
-            peak_area=quantity_or_none_from_unit(peak.area_unit, peak.area),
-            peak_width=quantity_or_none(TQuantityValueSecondTime, peak.width),
-            peak_height=quantity_or_none_from_unit(peak.height_unit, peak.height),
-            relative_peak_area=quantity_or_none(
-                TQuantityValuePercent, peak.relative_area
-            ),
-            relative_peak_height=quantity_or_none(
-                TQuantityValuePercent, peak.relative_height
-            ),
-            retention_time=quantity_or_none(
-                TQuantityValueSecondTime, peak.retention_time
-            ),
-            chromatographic_peak_resolution=quantity_or_none(
-                TQuantityValueUnitless, peak.chromatographic_peak_resolution
-            ),
-            chromatographic_peak_asymmetry_factor=quantity_or_none(
+                identifier=peak.identifier,
+                peak_index=peak.index,
+                peak_start=quantity_or_none_from_unit(peak.start_unit, peak.start),  # type: ignore[arg-type]
+                peak_end=quantity_or_none_from_unit(peak.end_unit, peak.end),  # type: ignore[arg-type]
+                peak_area=quantity_or_none_from_unit(peak.area_unit, peak.area),
+                peak_width=quantity_or_none(TQuantityValueSecondTime, peak.width),
+                peak_height=quantity_or_none_from_unit(peak.height_unit, peak.height),
+                relative_peak_area=quantity_or_none(
+                    TQuantityValuePercent, peak.relative_area
+                ),
+                relative_peak_height=quantity_or_none(
+                    TQuantityValuePercent, peak.relative_height
+                ),
+                retention_time=quantity_or_none(
+                    TQuantityValueSecondTime, peak.retention_time
+                ),
+                chromatographic_peak_resolution=quantity_or_none(
+                    TQuantityValueUnitless, peak.chromatographic_peak_resolution
+                ),
+                chromatographic_peak_asymmetry_factor=quantity_or_none(
                     TQuantityValueUnitless, peak.chromatographic_asymmetry
                 ),
-            peak_width_at_half_height=quantity_or_none(
-                TQuantityValueSecondTime, peak.peak_width_at_half_height
+                peak_width_at_half_height=quantity_or_none(
+                    TQuantityValueSecondTime, peak.peak_width_at_half_height
+                ),
+                asymmetry_factor_measured_at_10___height=quantity_or_none(
+                    TQuantityValueUnitless,
+                    peak.asymmetry_factor_measured_at_10___height,
+                ),
+                peak_width_at_5___of_height=quantity_or_none(
+                    TQuantityValueSecondTime, peak.peak_width_at_5___of_height
+                ),
+                peak_width_at_10___of_height=quantity_or_none(
+                    TQuantityValueSecondTime, peak.peak_width_at_10___of_height
+                ),
+                peak_width_at_baseline=quantity_or_none(
+                    TQuantityValueSecondTime, peak.peak_width_at_baseline
+                ),
+                number_of_theoretical_plates__chromatography_=quantity_or_none(
+                    TQuantityValueUnitless,
+                    peak.number_of_theoretical_plates__chromatography_,
+                ),
+                written_name=peak.written_name,
             ),
-            asymmetry_factor_measured_at_10___height=quantity_or_none(
-                TQuantityValueUnitless, peak.asymmetry_factor_measured_at_10___height
-            ),
-            peak_width_at_5___of_height=quantity_or_none(
-                TQuantityValueSecondTime, peak.peak_width_at_5___of_height
-            ),
-            peak_width_at_10___of_height=quantity_or_none(
-                TQuantityValueSecondTime, peak.peak_width_at_10___of_height
-            ),
-            peak_width_at_baseline=quantity_or_none(
-                TQuantityValueSecondTime, peak.peak_width_at_baseline
-            ),
-            number_of_theoretical_plates__chromatography_=quantity_or_none(
-                TQuantityValueUnitless,
-                peak.number_of_theoretical_plates__chromatography_,
-            ),
-            written_name=peak.written_name,
-        ),
-        peak.custom_info,
+            peak.custom_info,
         )
 
     def _get_processed_data_aggregate_document(
@@ -426,11 +426,11 @@ class Mapper(SchemaMapper[Data, Model]):
         return add_custom_information_document(
             DeviceControlDocumentItem(
                 device_type=device_control_doc.device_type,
-            start_time_setting=(
-                self.get_date_time(device_control_doc.start_time)
-                if device_control_doc.start_time is not None
-                else None
-            ),
+                start_time_setting=(
+                    self.get_date_time(device_control_doc.start_time)
+                    if device_control_doc.start_time is not None
+                    else None
+                ),
                 device_identifier=device_control_doc.device_identifier,
                 detection_type=device_control_doc.detection_type,
                 product_manufacturer=device_control_doc.product_manufacturer,
