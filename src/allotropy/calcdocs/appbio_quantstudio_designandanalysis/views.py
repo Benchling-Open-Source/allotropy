cf5c11446c98fc63029a589fd80d420673be9f6a
Alejandro Salgado <alejando.salgado@benchling.com>
2025-01-29 11:07:57 -0500

    implement view with reference for quantstudio design in calcdocs

--- src/allotropy/calcdocs/appbio_quantstudio_designandanalysis/views.py
+++ src/allotropy/calcdocs/appbio_quantstudio_designandanalysis/views.py
@@ -1,12 +1,35 @@
+from __future__ import annotations
+
 from collections import defaultdict
 
 from allotropy.calcdocs.extractor import Element
-from allotropy.calcdocs.view import View
+from allotropy.calcdocs.view import Keys, View
 
 
-class SampleView(View):
-    def __init__(self, sub_view: View | None = None):
-        super().__init__(name="sample_id", sub_view=sub_view)
+class ViewWithReference(View):
+    def __init__(
+        self,
+        name: str,
+        sub_view: View | None,
+        reference: str | None = None,
+    ):
+        super().__init__(name, sub_view)
+        self.reference = reference
+
+    def filter_keys(self, keys: Keys) -> Keys:
+        filtered_keys = super().filter_keys(keys)
+        if self.reference is not None and filtered_keys.get_or_none(self.name):
+            return filtered_keys.overwrite(self.name, self.reference)
+        return filtered_keys
+
+
+class SampleView(ViewWithReference):
+    def __init__(
+        self,
+        sub_view: View | None = None,
+        reference: str | None = None,
+    ):
+        super().__init__(name="sample_id", sub_view=sub_view, reference=reference)
 
     def sort_elements(self, elements: list[Element]) -> dict[str, list[Element]]:
         items = defaultdict(list)
@@ -16,15 +39,19 @@ class SampleView(View):
         return dict(items)
 
 
-class TargetView(View):
-    def __init__(self, sub_view: View | None = None):
-        super().__init__(name="target_dna", sub_view=sub_view)
+class TargetView(ViewWithReference):
+    def __init__(
+        self,
+        sub_view: View | None = None,
+        reference: str | None = None,
+    ):
+        super().__init__(name="target_dna", sub_view=sub_view, reference=reference)
 
     def sort_elements(self, elements: list[Element]) -> dict[str, list[Element]]:
         items = defaultdict(list)
         for element in elements:
-            if target_dna_description := element.get_str("target_dna_description"):
-                items[str(target_dna_description)].append(element)
+            if target_dna := element.get_str("target_dna_description"):
+                items[str(target_dna)].append(element)
         return dict(items)
 
 
