0c36911b92ae4cfc7b60fd6158121efe08b3e96b
Joshua Hernandez <jherna@benchling.com>
2025-01-30 15:30:36 -0500

    --ammend

--- src/allotropy/parsers/luminex_xponent/constants.py
+++ src/allotropy/parsers/luminex_xponent/constants.py
@@ -3,7 +3,7 @@ DEFAULT_CONTAINER_TYPE = "well plate"
 DEFAULT_DEVICE_TYPE = "multi analyte profiling analyzer"
 
 LUMINEX_EMPTY_PATTERN = r"^[,\"\s]*$"
-CALIBRATION_BLOCK_HEADER = "Most Recent Calibration and (Control|Verification) Results:"
+CALIBRATION_BLOCK_HEADER = "Most Recent Calibration and (Control|Verification) Results"
 TABLE_HEADER_PATTERN = '^"?DataType:"?,"?([^,"]*)"?'
 MINIMUM_CALIBRATION_LINE_COLS = 2
 EXPECTED_CALIBRATION_RESULT_LEN = 2
