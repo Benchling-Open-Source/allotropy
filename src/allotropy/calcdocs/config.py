848525834eaed6bf082cf94745accadd69cbe201
Alejandro Salgado <alejando.salgado@benchling.com>
2025-01-30 11:50:16 -0500

    represent keys as dataclasses in calcdocs

--- src/allotropy/calcdocs/config.py
+++ src/allotropy/calcdocs/config.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from itertools import chain
 
 from allotropy.calcdocs.extractor import Element
-from allotropy.calcdocs.view import ViewData
+from allotropy.calcdocs.view import Keys, ViewData
 from allotropy.parsers.utils.calculated_data_documents.definition import (
     CalculatedDocument,
     DataSource,
@@ -30,15 +30,14 @@ class CalculatedDataConfig:
     source_configs: list[CalculatedDataConfig | MeasurementConfig]
 
     def iter_data_sources(
-        self, parent_keys: dict[str, str], _: list[Element]
+        self, parent_keys: Keys, _: list[Element]
     ) -> Iterator[DataSource]:
         keys = self.view_data.filter_keys(parent_keys)
         item = self.view_data.get_item(keys)
-        empty: dict[str, str] = {}
-        sub_keys_iterator = item.iter_keys() if isinstance(item, ViewData) else [empty]
+        sub_keys_iterator = item.iter_keys() if isinstance(item, ViewData) else [Keys()]
 
         for sub_keys in sub_keys_iterator:
-            if calc_doc := self.get_calc_doc({**keys, **sub_keys}):
+            if calc_doc := self.get_calc_doc(keys.append(sub_keys)):
                 yield DataSource(
                     feature=calc_doc.name,
                     reference=calc_doc,
@@ -47,7 +46,7 @@ class CalculatedDataConfig:
 
     def get_calc_doc(
         self,
-        keys: dict[str, str],
+        keys: Keys,
     ) -> CalculatedDocument | None:
         elements = self.view_data.get_leaf_items(keys)
         value = elements[0].get_float_or_none(self.value)
@@ -85,7 +84,7 @@ class MeasurementConfig:
     value: str
 
     def iter_data_sources(
-        self, _: dict[str, str], elements: list[Element]
+        self, _: Keys, elements: list[Element]
     ) -> Iterator[DataSource]:
         for element in elements:
             if element.get_or_none(self.value) is not None:
