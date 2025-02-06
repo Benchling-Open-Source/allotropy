09b03f943321a5532fc52d1e528615bae747494f
Joshua Hernandez <jherna@benchling.com>
2025-02-06 15:21:12 -0500

    added field to its respective document level

--- src/allotropy/parsers/bmg_labtech_smart_control/constants.py
+++ src/allotropy/parsers/bmg_labtech_smart_control/constants.py
@@ -5,7 +5,7 @@ PRODUCT_MANUFACTURER = "BMG LABTECH"
 SOFTWARE_NAME = "SMART Control"
 DEVICE_TYPE = "fluorescence detector"
 
-TARGET_TEMPERATURE = "Target temperature [°C]:"
+TARGET_TEMPERATURE = "Target temperature [°C]"
 SAMPLE_ROLE_TYPE_MAPPING = {
     "Blank": SampleRoleType.blank_role,
     "Standard": SampleRoleType.standard_sample_role,
