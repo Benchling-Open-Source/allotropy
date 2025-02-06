f5380683f1db55cc317d8056c0d2eecaba3ad48c
Joshua Hernandez <jherna@benchling.com>
2025-02-06 12:51:21 -0500

    reverded change on fluorescense to avoid mixing the ticket

--- src/allotropy/allotrope/schema_mappers/adm/plate_reader/rec/_2024/_06/plate_reader.py
+++ src/allotropy/allotrope/schema_mappers/adm/plate_reader/rec/_2024/_06/plate_reader.py
@@ -416,47 +416,44 @@ class Mapper(SchemaMapper[Data, Model]):
             measurement_identifier=measurement.identifier,
             device_control_aggregate_document=DeviceControlAggregateDocument(
                 device_control_document=[
-                    add_custom_information_document(
-                        DeviceControlDocumentItem(
-                            device_type=measurement.device_type,
-                            detection_type=measurement.detection_type,
-                            detector_wavelength_setting=quantity_or_none(
-                                TQuantityValueNanometer,
-                                measurement.detector_wavelength_setting,
-                            ),
-                            detector_bandwidth_setting=quantity_or_none(
-                                TQuantityValueNanometer,
-                                measurement.detector_bandwidth_setting,
-                            ),
-                            excitation_wavelength_setting=quantity_or_none(
-                                TQuantityValueNanometer,
-                                measurement.excitation_wavelength_setting,
-                            ),
-                            excitation_bandwidth_setting=quantity_or_none(
-                                TQuantityValueNanometer,
-                                measurement.excitation_bandwidth_setting,
-                            ),
-                            wavelength_filter_cutoff_setting=quantity_or_none(
-                                TQuantityValueNanometer,
-                                measurement.wavelength_filter_cutoff_setting,
-                            ),
-                            detector_distance_setting__plate_reader_=quantity_or_none(
-                                TQuantityValueMillimeter,
-                                measurement.detector_distance_setting,
-                            ),
-                            scan_position_setting__plate_reader_=(
-                                measurement.scan_position_setting.value
-                                if measurement.scan_position_setting
-                                else None
-                            ),
-                            detector_gain_setting=measurement.detector_gain_setting,
-                            number_of_averages=quantity_or_none(
-                                TQuantityValueNumber, measurement.number_of_averages
-                            ),
-                            detector_carriage_speed_setting=measurement.detector_carriage_speed,
+                    DeviceControlDocumentItem(
+                        device_type=measurement.device_type,
+                        detection_type=measurement.detection_type,
+                        detector_wavelength_setting=quantity_or_none(
+                            TQuantityValueNanometer,
+                            measurement.detector_wavelength_setting,
                         ),
-                        measurement.device_control_custom_info,
-                    ),
+                        detector_bandwidth_setting=quantity_or_none(
+                            TQuantityValueNanometer,
+                            measurement.detector_bandwidth_setting,
+                        ),
+                        excitation_wavelength_setting=quantity_or_none(
+                            TQuantityValueNanometer,
+                            measurement.excitation_wavelength_setting,
+                        ),
+                        excitation_bandwidth_setting=quantity_or_none(
+                            TQuantityValueNanometer,
+                            measurement.excitation_bandwidth_setting,
+                        ),
+                        wavelength_filter_cutoff_setting=quantity_or_none(
+                            TQuantityValueNanometer,
+                            measurement.wavelength_filter_cutoff_setting,
+                        ),
+                        detector_distance_setting__plate_reader_=quantity_or_none(
+                            TQuantityValueMillimeter,
+                            measurement.detector_distance_setting,
+                        ),
+                        scan_position_setting__plate_reader_=(
+                            measurement.scan_position_setting.value
+                            if measurement.scan_position_setting
+                            else None
+                        ),
+                        detector_gain_setting=measurement.detector_gain_setting,
+                        number_of_averages=quantity_or_none(
+                            TQuantityValueNumber, measurement.number_of_averages
+                        ),
+                        detector_carriage_speed_setting=measurement.detector_carriage_speed,
+                    )
                 ]
             ),
             fluorescence=TQuantityValueRelativeFluorescenceUnit(
