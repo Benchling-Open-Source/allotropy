47cc7326038b76f103d2b591ae45d086aeae87bb
Alejandro Salgado <alejando.salgado@benchling.com>
2025-02-20 14:51:55 -0500

    refactor relative standard curve to use calcdocs library in quantstudio

--- src/allotropy/parsers/appbio_quantstudio/appbio_quantstudio_calculated_documents.py
+++ src/allotropy/parsers/appbio_quantstudio/appbio_quantstudio_calculated_documents.py
@@ -667,6 +667,24 @@ def ct_sd(view_data: NewViewData) -> CalculatedDataConfig:
     )
 
 
+def r_squared(view_data: NewViewData) -> CalculatedDataConfig:
+    return CalculatedDataConfig(
+        name="r^2",
+        value="r_squared",
+        view_data=view_data,
+        source_configs=(ctr(),),
+    )
+
+
+def efficiency(view_data: NewViewData) -> CalculatedDataConfig:
+    return CalculatedDataConfig(
+        name="efficiency",
+        value="efficiency",
+        view_data=view_data,
+        source_configs=(ctr(),),
+    )
+
+
 def rn_mean(view_data: NewViewData) -> CalculatedDataConfig:
     return CalculatedDataConfig(
         name="rn mean",
@@ -735,6 +753,42 @@ def delta_ct_se(
         value="delta_ct_se",
         view_data=view_data,
         source_configs=(ct_sd_conf, ref_ct_sd_conf),
+    )
+
+
+def relative_rq(
+    view_data: NewViewData,
+    quantity_mean_conf: CalculatedDataConfig,
+) -> CalculatedDataConfig:
+    return CalculatedDataConfig(
+        name="relative rq",
+        value="rq",
+        view_data=view_data,
+        source_configs=(quantity_mean_conf,),
+    )
+
+
+def relative_rq_min(
+    view_data: NewViewData,
+    rq_conf: CalculatedDataConfig,
+) -> CalculatedDataConfig:
+    return CalculatedDataConfig(
+        name="relative rq min",
+        value="rq_min",
+        view_data=view_data,
+        source_configs=(rq_conf,),
+    )
+
+
+def relative_rq_max(
+    view_data: NewViewData,
+    rq_conf: CalculatedDataConfig,
+) -> CalculatedDataConfig:
+    return CalculatedDataConfig(
+        name="relative rq max",
+        value="rq_max",
+        view_data=view_data,
+        source_configs=(rq_conf,),
     )
 
 
@@ -929,62 +983,58 @@ def iter_standard_curve_calc_docs(
 
 
 def iter_relative_standard_curve_calc_docs(
-    view_st_data: ViewData[WellItem],
-    view_tr_data: ViewData[WellItem],
+    well_items: list[WellItem],
 ) -> Iterator[CalculatedDocument]:
-    # Quantity, Quantity Mean, Quantity SD, Ct Mean, Ct SD, RQ, RQ min,
-    # RQ max, Y-Intercept, R(superscript 2), Slope, Efficiency,
-    # Amplification score, Cq confidence
-    calc_docs: list[CalculatedDocument | None] = []
-    for sample, target in view_st_data.iter_keys():
-        for well_item in view_st_data.get_leaf_item(sample, target):
-            calc_docs.append(build_quantity(view_tr_data, target, well_item))
+    # Y-Intercept, Slope Quantity, Amplification score, Cq confidence
+    # Quantity Mean, Quantity SD, Ct Mean, Ct SD
+    # Relative RQ, Relative RQ min, Relative RQ max,
+    # R(superscript 2), Efficiency
+    elements = AppbioQuantstudioExtractor.get_elements(well_items)
 
-    for sample, target in view_st_data.iter_keys():
-        for well_item in view_st_data.get_leaf_item(sample, target):
-            calc_docs.append(build_amp_score(well_item))
+    sid_tdna_view_data = NewSampleView(sub_view=NewTargetView()).apply(elements)
+    sid_tdna_uuid_view_data = NewSampleView(
+        sub_view=NewTargetView(sub_view=UuidView())
+    ).apply(elements)
+    tdna_view_data = NewTargetRoleView().apply(elements)
 
-    for sample, target in view_st_data.iter_keys():
-        for well_item in view_st_data.get_leaf_item(sample, target):
-            calc_docs.append(build_cq_conf(well_item))
+    quantity_conf = quantity(
+        sid_tdna_uuid_view_data,
+        y_intercept(tdna_view_data),
+        slope(tdna_view_data),
+    )
 
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(
-            build_quantity_mean(view_st_data, view_tr_data, sample, target)
-        )
+    configs = CalcDocsConfig(
+        [
+            quantity_conf,
+            amplification_score(sid_tdna_uuid_view_data),
+            cq_confidence(sid_tdna_uuid_view_data),
+            quantity_mean(sid_tdna_view_data, quantity_conf),
+            quantity_sd(sid_tdna_view_data, quantity_conf),
+            ct_mean(sid_tdna_view_data),
+            ct_sd(sid_tdna_view_data),
+            relative_rq_min(
+                sid_tdna_view_data,
+                relative_rq(
+                    sid_tdna_view_data,
+                    quantity_mean(sid_tdna_view_data, quantity_conf),
+                ),
+            ),
+            relative_rq_max(
+                sid_tdna_view_data,
+                relative_rq(
+                    sid_tdna_view_data,
+                    quantity_mean(sid_tdna_view_data, quantity_conf),
+                ),
+            ),
+            y_intercept(tdna_view_data),
+            r_squared(tdna_view_data),
+            slope(tdna_view_data),
+            efficiency(tdna_view_data),
+        ]
+    )
 
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(build_quantity_sd(view_st_data, view_tr_data, sample, target))
-
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(build_ct_mean(view_st_data, sample, target))
-
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(build_ct_sd(view_st_data, sample, target))
-
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(
-            build_relative_rq_min(view_st_data, view_tr_data, sample, target)
-        )
-
-    for sample, target in view_st_data.iter_keys():
-        calc_docs.append(
-            build_relative_rq_max(view_st_data, view_tr_data, sample, target)
-        )
-
-    for target in view_tr_data.data:
-        calc_docs.append(build_y_intercept(view_tr_data, target))
-
-    for target in view_tr_data.data:
-        calc_docs.append(build_r_squared(view_tr_data, target))
-
-    for target in view_tr_data.data:
-        calc_docs.append(build_slope(view_tr_data, target))
-
-    for target in view_tr_data.data:
-        calc_docs.append(build_efficiency(view_tr_data, target))
-
-    yield from yield_documents(calc_docs)
+    for calc_doc in configs.construct():
+        yield from calc_doc.iter_struct()
 
 
 def iter_presence_absence_calc_docs(
@@ -1026,10 +1076,7 @@ def iter_calculated_data_documents(
     view_tr_data = view_tr.apply(well_items)
 
     if experiment_type == ExperimentType.relative_standard_curve_qpcr_experiment:
-        yield from iter_relative_standard_curve_calc_docs(
-            view_st_data,
-            view_tr_data,
-        )
+        yield from iter_relative_standard_curve_calc_docs(well_items)
     elif experiment_type == ExperimentType.comparative_ct_qpcr_experiment:
         yield from iter_comparative_ct_calc_docs(
             well_items,
