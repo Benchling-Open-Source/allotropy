09a4fb3df961ca929a587f527c08870774b0a846
Joshua Hernandez <jherna@benchling.com>
2025-02-27 11:23:47 -0500

    migrate smart control to new calc data library

--- src/allotropy/parsers/bmg_labtech_smart_control/bmg_labtech_smart_control_structure.py
+++ src/allotropy/parsers/bmg_labtech_smart_control/bmg_labtech_smart_control_structure.py
@@ -1,11 +1,11 @@
 import logging
 from pathlib import Path
 
-from allotropy.allotrope.models.shared.components.plate_reader import SampleRoleType
 from allotropy.allotrope.models.shared.definitions.custom import (
     TQuantityValueSecondTime,
     TQuantityValueUnitless,
 )
+from allotropy.allotrope.models.shared.definitions.units import RelativeFluorescenceUnit
 from allotropy.allotrope.schema_mappers.adm.plate_reader.rec._2024._06.plate_reader import (
     CalculatedDataItem,
     DataSource,
@@ -14,6 +14,19 @@ from allotropy.allotrope.schema_mappers.adm.plate_reader.rec._2024._06.plate_rea
     MeasurementType,
     Metadata,
     ScanPositionSettingPlateReader,
+)
+from allotropy.calcdocs.bmg_labtech_smart_control.extractor import (
+    BmgLabtechSmartControlExtractor,
+    CalculatedDataMeasurementStructure,
+)
+from allotropy.calcdocs.bmg_labtech_smart_control.views import (
+    BlankRoleTypeView,
+    CorrectedView,
+)
+from allotropy.calcdocs.config import (
+    CalcDocsConfig,
+    CalculatedDataConfig,
+    MeasurementConfig,
 )
 from allotropy.parsers.bmg_labtech_smart_control.bmg_labtech_smart_control_reader import (
     BmgLabtechSmartControlReader,
@@ -26,6 +39,9 @@ from allotropy.parsers.bmg_labtech_smart_control.constants import (
     TARGET_TEMPERATURE,
 )
 from allotropy.parsers.constants import NOT_APPLICABLE
+from allotropy.parsers.utils.calculated_data_documents.definition import (
+    CalculatedDocument,
+)
 from allotropy.parsers.utils.pandas import SeriesData
 from allotropy.parsers.utils.uuids import random_uuid_str
 from allotropy.parsers.utils.values import quantity_or_none, try_float
@@ -134,62 +150,74 @@ def map_measurement_group(row: SeriesData, headers: SeriesData) -> MeasurementGr
 def create_calculated_data_documents(
     measurement_groups: list[MeasurementGroup], reader: BmgLabtechSmartControlReader
 ) -> list[CalculatedDataItem] | None:
-    blank_measurements = [
-        group.measurements[0]
-        for group in measurement_groups
-        if group.measurements[0].sample_role_type == SampleRoleType.blank_role
-    ]
-
-    if not blank_measurements:
-        return []
-
     if not reader.average_of_blank_used:
         logging.warning(
             "Microplate endpoint sheet does not contain average of blanks used value but blank measurements are present."
         )
         return []
-    non_blank_measurements = [
-        measurement
-        for group in measurement_groups
-        for measurement in group.measurements
-        if measurement.sample_role_type != SampleRoleType.blank_role
-    ]
-
-    blank_average = reader.average_of_blank_used
-    average_calc_document = CalculatedDataItem(
-        identifier=random_uuid_str(),
-        name="Average of all blanks used",
-        value=blank_average,
-        data_sources=[
-            DataSource(
-                identifier=measurement.identifier,
-                feature="fluorescence",
-            )
-            for measurement in blank_measurements
-        ],
-        unit="RFU",
-    )
-    blank_corrected_calc_documents: list[CalculatedDataItem] = []
-    for idx, measurement in enumerate(non_blank_measurements):
-        corrected_value = SeriesData(reader.data.iloc[idx])[
+    # adapter only supports one measurement per group
+    measurements = [group.measurements[0] for group in measurement_groups]
+    calc_data_measurements = []
+    for idx, measurement in enumerate(measurements):
+        corrected_value = SeriesData(reader.data.iloc[idx]).get(
             float, "Blank corrected based on Raw Data (480-14/520-30)"
-        ]
-        blank_corrected_calc_documents.append(
-            CalculatedDataItem(
-                identifier=random_uuid_str(),
-                name="Blank corrected based on Raw Data (480-14/520-30)",
-                value=corrected_value,
-                data_sources=[
-                    DataSource(
-                        identifier=measurement.identifier,
-                        feature="fluorescence",
-                    ),
-                    DataSource(
-                        identifier=average_calc_document.identifier,
-                        feature="Average of all blanks used",
-                    ),
-                ],
-                unit="RFU",
+        )
+        calc_data_measurements.append(
+            CalculatedDataMeasurementStructure(
+                measurement=measurement,
+                corrected_value=corrected_value,
+                average_of_blank_used=reader.average_of_blank_used,
             )
         )
-    return [average_calc_document, *blank_corrected_calc_documents]
+
+    elements = BmgLabtechSmartControlExtractor.get_elements(calc_data_measurements)
+    corrected_view_data = CorrectedView().apply(elements=elements)
+    role_type_view_data = BlankRoleTypeView().apply(elements=elements)
+
+    measurement_conf = MeasurementConfig(
+        name="fluorescence",
+        value="fluorescence",
+    )
+    average_of_blank_used_conf = CalculatedDataConfig(
+        name="Average of all blanks used",
+        value="average_of_blank_used",
+        view_data=role_type_view_data,
+        source_configs=(measurement_conf,),
+        unit=RelativeFluorescenceUnit.unit,
+    )
+
+    corrected_conf = CalculatedDataConfig(
+        name="Blank corrected based on Raw Data (480-14/520-30)",
+        value="corrected_value",
+        view_data=corrected_view_data,
+        source_configs=(measurement_conf, average_of_blank_used_conf),
+        unit=RelativeFluorescenceUnit.unit,
+    )
+
+    configs = CalcDocsConfig([average_of_blank_used_conf, corrected_conf])
+
+    calc_docs = [
+        calc_doc
+        for parent_calc_doc in configs.construct()
+        for calc_doc in parent_calc_doc.iter_struct()
+    ]
+    return _map_calc_docs_to_calculated_data_items(calc_docs)
+
+
+# TODO: standardize the calcuated data items for plate reader
+def _map_calc_docs_to_calculated_data_items(
+    calc_docs: list[CalculatedDocument],
+) -> list[CalculatedDataItem]:
+    return [
+        CalculatedDataItem(
+            identifier=calc_doc.uuid,
+            name=calc_doc.name,
+            value=calc_doc.value,
+            data_sources=[
+                DataSource(feature=source.feature, identifier=source.reference.uuid)
+                for source in calc_doc.data_sources
+            ],
+            unit=calc_doc.unit,
+        )
+        for calc_doc in calc_docs
+    ]
