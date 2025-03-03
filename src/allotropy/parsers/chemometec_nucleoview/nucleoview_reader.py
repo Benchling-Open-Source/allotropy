33c7ba3dcf68dff67bd3ddedf3fe3fe31ae0a1aa
Nathan Stender <nathan.stender@benchling.com>
2025-03-03 14:01:52 -0500

    Fix naming

--- src/allotropy/parsers/chemometec_nucleoview/nucleoview_reader.py
+++ src/allotropy/parsers/chemometec_nucleoview/nucleoview_reader.py
@@ -9,7 +9,7 @@ class NucleoviewReader:
 
     @classmethod
     def read(cls, contents: IOType) -> pd.DataFrame:
-        df = read_csv(contents, sep="[;,]+", skipinitialspace=True, index_col=0)
+        df = read_csv(contents, sep="[;,]+", engine="python", skipinitialspace=True, index_col=0)
         df = df[:-1].dropna(axis="index", how="all").T
         df = df.rename(
             {"Estimated cell diameter [um]": "Estimated cell diameter (um)"},
--- src/allotropy/parsers/chemometec_nucleoview/nucleoview_structure.py
+++ src/allotropy/parsers/chemometec_nucleoview/nucleoview_structure.py
@@ -49,7 +49,7 @@ def create_metadata(data: SeriesData, file_path: str) -> Metadata:
         software_version=data.get(str, "Application SW version"),
         device_type=NUCLEOCOUNTER_DEVICE_TYPE,
         detection_type=NUCLEOCOUNTER_DETECTION_TYPE,
-        custom_data_system_info=data.get_custom_keys(
+        system_info_custom_info=data.get_custom_keys(
             {
                 "csv file version",
                 "21 CFR Part 11",
@@ -102,7 +102,7 @@ def create_measurement_groups(
     )
     measurement_group = MeasurementGroup(
         analyst=data.get(str, "Operator", DEFAULT_ANALYST),
-        custom_info_doc=data.get_custom_keys(MEASUREMENT_AGG_DOCUMENT_CUSTOM_FIELDS),
+        custom_info=data.get_custom_keys(MEASUREMENT_AGG_DOCUMENT_CUSTOM_FIELDS),
         measurements=[
             Measurement(
                 measurement_identifier=random_uuid_str(),
