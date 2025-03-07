eadbb46e26ab013613f353ebce8569993b3477cf
Joshua Hernandez <jherna@benchling.com>
2025-03-07 14:21:47 -0500

    Add misisng view

--- src/allotropy/calcdocs/msd_workbench/extractor.py
+++ src/allotropy/calcdocs/msd_workbench/extractor.py
@@ -1,5 +1,5 @@
 from allotropy.calcdocs.extractor import Element, Extractor
-from allotropy.calcdocs.msd_workbench.calculdated_data_structure import (
+from allotropy.parsers.msd_workbench.calculdated_data_structure import (
     CalculatedDataMeasurementStructure,
 )
 
@@ -12,7 +12,7 @@ class MsdWorkbenchExtractor(Extractor[CalculatedDataMeasurementStructure]):
         return Element(
             uuid=calc_data_structure.measurement.identifier,
             data={
-                "type_": calc_data_structure.measurement.type_,
+                "type_": calc_data_structure.measurement.type_.value,
                 "uuid": calc_data_structure.measurement.identifier,
                 "luminescence": calc_data_structure.measurement.luminescence,
                 "sample_identifier": calc_data_structure.measurement.sample_identifier,
@@ -22,10 +22,14 @@ class MsdWorkbenchExtractor(Extractor[CalculatedDataMeasurementStructure]):
                 "device_type": calc_data_structure.measurement.device_type,
                 "detection_type": calc_data_structure.measurement.detection_type,
                 "mass_concentration": calc_data_structure.measurement.mass_concentration,
-                "sample_role_type": calc_data_structure.measurement.sample_role_type,
+                "sample_role_type": calc_data_structure.measurement.sample_role_type.value
+                if calc_data_structure.measurement.sample_role_type
+                else None,
                 "assay_identifier": calc_data_structure.measurement.measurement_custom_info.get(
                     "assay identifier"
-                ),
+                )
+                if calc_data_structure.measurement.measurement_custom_info
+                else None,
                 "adjusted_signal": calc_data_structure.adjusted_signal,
                 "mean": calc_data_structure.mean,
                 "adj_sig_mean": calc_data_structure.adj_sig_mean,
