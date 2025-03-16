99701fa13d96fd643c10b43209da0c2ecc6666c4
Nathan Stender <nathan.stender@benchling.com>
2025-03-16 18:19:52 -0400

    Handle multiple spots

--- src/allotropy/parsers/methodical_mind/methodical_mind_reader.py
+++ src/allotropy/parsers/methodical_mind/methodical_mind_reader.py
@@ -1,7 +1,9 @@
 from collections.abc import Iterator
+import re
 
 import pandas as pd
 
+from allotropy.exceptions import AllotropeConversionError
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.lines_reader import CsvReader, read_to_lines
 from allotropy.parsers.utils.pandas import SeriesData
@@ -12,6 +14,7 @@ class MethodicalMindReader:
     SUPPORTED_EXTENSIONS = "txt"
     plate_headers: list[SeriesData]
     plate_data: list[pd.DataFrame]
+    spots: list[int]
 
     def __init__(self, named_file_contents: NamedFileContents) -> None:
         file_lines = read_to_lines(named_file_contents)
@@ -19,8 +22,29 @@ class MethodicalMindReader:
 
         self.plate_headers = []
         self.plate_data = []
+        self.spots = []
         while reader.current_line_exists():
-            lines = list(reader.pop_until(r"Data|Spot\s+:"))
+            lines = list(reader.pop_until("Data"))
+
+            # Find spot legend if it exists
+            for i, line in enumerate(lines):
+                if (
+                    line.startswith("Spot")
+                    and i + 1 < len(lines)
+                    and lines[i + 1].startswith("Legend")
+                ):
+                    end = i + 2
+                    while end < len(lines):
+                        if not lines[end].startswith(""):
+                            break
+                        end += 1
+                    spot_legend = "\n".join(lines[i : end - 1])
+                    lines = lines[:i] + lines[end - 1 :]
+                    self.spots = [
+                        int(spot) for spot in re.findall(r"(\d+)", spot_legend)
+                    ]
+                    break
+
             lines = [
                 line.replace("\\t", "\t")
                 for line in lines
@@ -31,10 +55,6 @@ class MethodicalMindReader:
                 key.strip(): value.strip() for key, value in kv_pairs if value.strip()
             }
             self.plate_headers.append(SeriesData(pd.Series(key_values)))
-
-            if "Data" not in reader.get():
-                # TODO get visible spots and pass them to create location ids
-                reader.drop_until("Data")
 
             # Pop data title line
             reader.pop()
@@ -57,5 +77,13 @@ class MethodicalMindReader:
 
             reader.drop_until("Stack ID")
 
-    def __iter__(self) -> Iterator[tuple[SeriesData, pd.DataFrame]]:
+        if (
+            self.spots
+            and len(self.plate_data) > 1
+            and len(self.plate_data) != len(self.spots)
+        ):
+            msg = f"Unrecognized file format for MSD parser: spot legend reports {len(self.spots)} spots, but found {len(self.data)} data tables."
+            raise AllotropeConversionError(msg)
+
+    def __iter__(self) -> Iterator[tuple[SeriesData, pd.DataFrame, int | None]]:
         yield from zip(self.plate_headers, self.plate_data, strict=True)
