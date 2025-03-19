8e8eda54a284da7075aca249a63f2a10c9922e6d
Alejandro Salgado <alejando.salgado@benchling.com>
2025-03-19 16:31:50 -0500

    work in progress

--- src/allotropy/parsers/perkin_elmer_envision/perkin_elmer_envision_calcdocs.py
+++ src/allotropy/parsers/perkin_elmer_envision/perkin_elmer_envision_calcdocs.py
@@ -1,4 +1,8 @@
 from allotropy.allotrope.models.shared.definitions.units import UNITLESS
+from allotropy.calcdocs.config import (
+    CalculatedDataConfig,
+    MeasurementConfig,
+)
 from allotropy.parsers.perkin_elmer_envision.perkin_elmer_envision_structure import (
     PlateList,
     ReadType,
@@ -13,6 +17,25 @@ from allotropy.parsers.utils.calculated_data_documents.definition import (
 def create_calculated_data(
     plate_list: PlateList, read_type: ReadType
 ) -> list[CalculatedDocument]:
+
+    meas_config = MeasurementConfig(
+        name=read_type.value,
+        value="value",
+    )
+
+    for calculated_plate in plate_list.calculated:
+        CalculatedDataConfig(
+            name=calculated_plate.plate_info.name,
+            description=calculated_plate.plate_info.formula,
+            value="value",
+            view_data=view_data,
+            source_configs=(meas_config,),
+        )
+
+        for calculated_result, source_results in calculated_plate.get_result_and_sources(
+            plate_list
+        )
+
     return [
         CalculatedDocument(
             uuid=calculated_result.uuid,
