6dc8c44aa1a65a49343a7ff07ee2b4382b460509
Alejandro Salgado <alejando.salgado@benchling.com>
2025-03-27 11:02:51 -0500

    add calcdocs implementation to envision

--- src/allotropy/parsers/perkin_elmer_envision/perkin_elmer_envision_calcdocs.py
+++ src/allotropy/parsers/perkin_elmer_envision/perkin_elmer_envision_calcdocs.py
@@ -1,36 +1,46 @@
 from allotropy.allotrope.models.shared.definitions.units import UNITLESS
+from allotropy.calcdocs.config import (
+    CalcDocsConfig,
+    MeasurementConfig,
+)
+from allotropy.calcdocs.perkin_elmer_envision.config import EnvisionCalculatedDataConfig
+from allotropy.calcdocs.perkin_elmer_envision.views import PosView
 from allotropy.parsers.perkin_elmer_envision.perkin_elmer_envision_structure import (
     PlateList,
     ReadType,
 )
 from allotropy.parsers.utils.calculated_data_documents.definition import (
     CalculatedDocument,
-    DataSource,
-    Referenceable,
 )
-from allotropy.parsers.utils.uuids import random_uuid_str
 
 
 def create_calculated_data(
     plate_list: PlateList, read_type: ReadType
 ) -> list[CalculatedDocument]:
+    pos_view = PosView()
+
+    meas_config = MeasurementConfig(
+        name=read_type.value,
+        value="value",
+    )
+
+    configs = CalcDocsConfig(
+        [
+            EnvisionCalculatedDataConfig(
+                name=calculated_plate.plate_info.name,
+                description=calculated_plate.plate_info.formula,
+                value="calc value",
+                view_data=pos_view.special_apply(plate_list, calculated_plate),
+                source_configs=(meas_config,),
+                unit=UNITLESS,
+                plate_number=calculated_plate.plate_info.number,
+            )
+            for calculated_plate in plate_list.calculated
+        ]
+    )
+
     return [
-        CalculatedDocument(
-            uuid=random_uuid_str(),
-            name=calculated_plate.plate_info.name,
-            description=calculated_plate.plate_info.formula,
-            value=calculated_result.value,
-            unit=UNITLESS,
-            data_sources=[
-                DataSource(
-                    reference=Referenceable(source_result.uuid),
-                    feature=read_type.value,
-                )
-                for source_result in source_results
-            ],
-        )
-        for calculated_plate in plate_list.calculated
-        for calculated_result, source_results in calculated_plate.get_result_and_sources(
-            plate_list
-        )
+        calc_doc
+        for parent_calc_doc in configs.construct()
+        for calc_doc in parent_calc_doc.iter_struct()
     ]
