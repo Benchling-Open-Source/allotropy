0f7553dc87f9ea69ec7e869da63c7c86f8c081d4
Joshua Hernandez <jherna@benchling.com>
2025-04-03 17:38:55 -0500

    added error handle for novabio flex

--- src/allotropy/parsers/novabio_flex2/novabio_flex2_parser.py
+++ src/allotropy/parsers/novabio_flex2/novabio_flex2_parser.py
@@ -7,6 +7,7 @@ from allotropy.allotrope.schema_mappers.adm.solution_analyzer.benchling._2024._0
     Data,
     Mapper,
 )
+from allotropy.exceptions import AllotropeConversionError
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.novabio_flex2.novabio_flex2_structure import (
     create_measurement_groups,
@@ -27,9 +28,13 @@ class NovaBioFlexParser(VendorParser[Data, Model]):
 
     def create_data(self, named_file_contents: NamedFileContents) -> Data:
         # NOTE: calling parse_dates and removing NaN clears empty rows.
-        data = read_csv(
-            named_file_contents.contents, parse_dates=["Date & Time"]
-        ).replace(np.nan, None)
+        try:
+            data = read_csv(
+                named_file_contents.contents, parse_dates=["Date & Time"]
+            ).replace(np.nan, None)
+        except Exception as e:
+            msg = "NovaBio Flex2 parser requires 'Date & Time' column to be present in the CSV file."
+            raise AllotropeConversionError(msg) from e
 
         title = Title.create(named_file_contents.original_file_path)
         sample_data = SampleData.create(data)
