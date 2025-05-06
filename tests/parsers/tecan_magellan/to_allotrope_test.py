f71c68db64c12c1c7f07235347561433786e4203
Sebastian Lopez <sebastian.cardona@benchling.com>
2025-05-06 16:52:05 -0500

    check for well positions column in reader

--- tests/parsers/tecan_magellan/to_allotrope_test.py
+++ tests/parsers/tecan_magellan/to_allotrope_test.py
@@ -1,5 +1,8 @@
+import pytest
+
+from allotropy.exceptions import AllotropeConversionError
 from allotropy.parser_factory import Vendor
-from allotropy.testing.utils import get_testdata_dir
+from allotropy.testing.utils import from_file, get_testdata_dir
 from tests.to_allotrope_test import ParserTest
 
 VENDOR_TYPE = Vendor.TECAN_MAGELLAN
@@ -8,3 +11,10 @@ TESTDATA = get_testdata_dir(__file__)
 
 class TestParser(ParserTest):
     VENDOR = VENDOR_TYPE
+
+
+def test_missing_well_positions_column() -> None:
+    with pytest.raises(
+        AllotropeConversionError, match="Missing well positions column from the file."
+    ):
+        from_file(f"{TESTDATA}/errors/no_well_positions.xlsx", VENDOR_TYPE)
