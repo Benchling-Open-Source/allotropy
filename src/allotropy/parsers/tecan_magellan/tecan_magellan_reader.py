1918c04993cc1fe7a88bfde5c30d10a0e3b9d1cc
Sebastian Lopez <sebastian.cardona@benchling.com>
2025-05-09 10:58:34 -0500

    feat: Tecan Magellan - check for well positions column in reader (#983)
    Signed-off-by: Sebastian Lopez <sebastian.cardona@benchling.com>
    Co-authored-by: Nathan Stender <nathan.stender@benchling.com>

--- src/allotropy/parsers/tecan_magellan/tecan_magellan_reader.py
+++ src/allotropy/parsers/tecan_magellan/tecan_magellan_reader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+from allotropy.exceptions import AllotropeConversionError
 from allotropy.named_file_contents import NamedFileContents
 from allotropy.parsers.utils.pandas import read_excel, split_dataframe
 from allotropy.parsers.utils.values import assert_not_none
@@ -18,6 +19,9 @@ class TecanMagellanReader:
             lambda row: row.astype(str).iloc[0].startswith("Date of measurement"),
             include_split_row=True,
         )
+        if "Well positions" not in data:
+            msg = "File is missing required 'Well positions' column."
+            raise AllotropeConversionError(msg)
         # Files might contain rows with metadata that we will not use, we identify
         # those by checking that the Well positions have the right format
         data = data.dropna(subset=["Well positions"])
