bb95089e40798a37add55d4356bb6735f328a15f
joshua-benchling <jherna@benchling.com>
2025-06-11 10:18:01 -0500

    feat: Molecular Devices SoftMax Pro - update adapter to plate reader schema REC 25/03 (#1011)
    Co-authored-by: Nathan Stender <nathan.stender@benchling.com>

--- src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
+++ src/allotropy/parsers/moldev_softmax_pro/softmax_pro_parser.py
@@ -1,5 +1,5 @@
-from allotropy.allotrope.models.adm.plate_reader.rec._2024._06.plate_reader import Model
-from allotropy.allotrope.schema_mappers.adm.plate_reader.rec._2024._06.plate_reader import (
+from allotropy.allotrope.models.adm.plate_reader.rec._2025._03.plate_reader import Model
+from allotropy.allotrope.schema_mappers.adm.plate_reader.rec._2025._03.plate_reader import (
     Data,
     Mapper,
 )
