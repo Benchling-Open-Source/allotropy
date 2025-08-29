2f592ed9826d55b7a30bf2a290957fe0cbe11ad5
Sebastian Lopez <sebastian.cardona@benchling.com>
2025-08-29 09:52:53 -0500

    update instrument table

--- src/allotropy/parsers/cytiva_biacore_insight/cytiva_biacore_insight_parser.py
+++ src/allotropy/parsers/cytiva_biacore_insight/cytiva_biacore_insight_parser.py
@@ -21,7 +21,7 @@ from allotropy.parsers.vendor_parser import VendorParser
 
 class CytivaBiacoreInsightParser(VendorParser[MapperData, Model]):
     DISPLAY_NAME = DISPLAY_NAME
-    RELEASE_STATE = ReleaseState.WORKING_DRAFT
+    RELEASE_STATE = ReleaseState.RECOMMENDED
     SUPPORTED_EXTENSIONS = CytivaBiacoreInsightReader.SUPPORTED_EXTENSIONS
     SCHEMA_MAPPER = Mapper
 
