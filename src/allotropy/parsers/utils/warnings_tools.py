b4e89139cfc7b91d13301786966e98079e084290
Felipe Narvaez <felipe.narvaez@benchling.com>
2025-09-09 13:00:50 -0500

    added docstrig to suppress warnings decorator

--- src/allotropy/parsers/utils/warnings_tools.py
+++ src/allotropy/parsers/utils/warnings_tools.py
@@ -8,6 +8,12 @@ from typing import Any
 
 
 def suppress_unused_keys_warning(func: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Use when a class that warns for unread keys is created for a special one-off use that does not intend to read all keys, e.g.
+    to read a single value and return.
+    Not appropriate to use in the main use of the class where unread keys should be copied into custom data fields.
+    """
+
     @functools.wraps(func)
     def _wrapper(*args: Any, **kwargs: Any) -> Any:
         previous_warn_unused_keys = os.environ.pop("WARN_UNUSED_KEYS", None)
