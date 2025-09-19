df24d506bde85693ed99211a1abf0f0c4eee0553
Oscar Castro <oscar.castro@benchling.com>
2025-09-19 10:38:03 -0500

    feat: Migrate ctl_immunospot to use SeriesData.get_unread (#1076)
    Co-authored-by: Nathan Stender <nathan.stender@benchling.com>

--- src/allotropy/parsers/utils/pandas.py
+++ src/allotropy/parsers/utils/pandas.py
@@ -279,6 +279,7 @@ class SeriesData:
         # "++" can cause re.compile to fail. Since it is never a valid regex expression
         # it is safe to escape it to prevent the error.
         match_key = match_key.replace("++", r"\+\+")
+
         return bool(re.fullmatch(match_key, key))
 
     def _get_matching_keys(self, key_or_keys: str | set[str]) -> set[str]:
